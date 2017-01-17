e40fd977923a0245c0aa6c0959e1ea16b76cedaf
Ryan Hughes <ryanhughes624@gmail.com>
2017-01-17 11:45:39 +0200

    thanks to @tw3, fixed issue #15

--- src_users/reducers/index.js
+++ src_users/reducers/index.js
@@ -6,6 +6,16 @@ import users from "./users";
 // main reducers
 export const reducers = combineReducers({
   routing: routerReducer,
-  form: formReducer,
+  form: formReducer.plugin({
+    "user_edit": (state, action) => {
+      // reset form (wipe state) when navigating away from the User edit page
+      switch(action.type) {
+        case "@@router/LOCATION_CHANGE":
+          return undefined;
+        default:
+          return state;
+      }
+    }
+  }),
   users: users,
 });
