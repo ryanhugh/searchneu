7d43d1f49be08710915d7293d5a71296348c5d00
Ryan Hughes <ryanhughes624@gmail.com>
2017-02-11 19:00:56 -0800

    hmr works.

--- frontend/router.js
+++ frontend/router.js
@@ -7,12 +7,10 @@ import NotFound from "./components/NotFound";
 
 // build the router
 const router = (
-  <Router onUpdate={() => window.scrollTo(0, 0)} history={history}>
     <Route path="/" component={App}>
       <IndexRoute component={Home}/>
       <Route path="*" component={NotFound}/>
     </Route>
-  </Router>
 );
 
 // export
