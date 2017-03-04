302d211dcccf4075993c32c401608fbbd422f6ba
Ryan Hughes <ryanhughes624@gmail.com>
2017-03-03 20:46:45 -0800

    eslint is gooooooood.

--- scrapers/courseproData.js
+++ scrapers/courseproData.js
@@ -1,131 +1,123 @@
-import path from 'path'
-import fs from 'fs-promise'
-import mkdirp from 'mkdirp-promise'
-import request from 'request-promise-native'
+import path from 'path';
+import fs from 'fs-promise';
+import mkdirp from 'mkdirp-promise';
+import request from 'request-promise-native';
 
-const PUBLIC_DIR = 'compiled_frontend'
+const PUBLIC_DIR = 'compiled_frontend';
 
 
-async function fireRequest(url, body = {}, method = "POST") {
-	console.log(url, method)
+async function fireRequest(url, body = {}, method = 'POST') {
+  console.log(url, method);
 
-	url = 'https://coursepro.io' + url
+  url = `https://coursepro.io${url}`;
 
-	var options = {
-		method: method,
-		headers: {
-			'user-agent': "hi there"
-		},
-		json: true,
-		url: url
-	}
+  const options = {
+    method: method,
+    headers: {
+      'user-agent': 'hi there',
+    },
+    json: true,
+    url: url,
+  };
 
-	if (method == 'POST') {
-		options.body = body;
-	}
+  if (method === 'POST') {
+    options.body = body;
+  }
 
-	return await request(options)
+  return request(options);
 }
 
 //TODO: this needs to be a Key.js not this ghetto thing
 function getHash(obj) {
-	var keys = ['host', 'termId', 'subject', 'classUid', 'crn']
-	var retVal = [];
-	keys.forEach(function (key) {
-		if (!obj[key]) {
-			return;
-		}
-		retVal.push(obj[key].replace('/', '_'))
-	})
-	return retVal.join('/')
+  const keys = ['host', 'termId', 'subject', 'classUid', 'crn'];
+  const retVal = [];
+  keys.forEach((key) => {
+    if (!obj[key]) {
+      return;
+    }
+    retVal.push(obj[key].replace('/', '_'));
+  });
+  return retVal.join('/');
 }
 
 
 async function main() {
+  const hosts = await fireRequest('/listColleges');
 
-	var hosts = await fireRequest('/listColleges')
+  console.log(hosts);
+  let promises = [];
 
-	console.log(hosts)
-	var promises = [];
+  hosts.forEach((host) => {
+    promises.push(fireRequest('/listTerms', {
+      host: host.host.replace('/', '_'),
+    }));
+  });
 
-	hosts.forEach(function (host) {
-		promises.push(fireRequest('/listTerms', {
-			host: host.host.replace('/', '_')
-		}))
-	})
+  let terms = await Promise.all(promises);
 
-	var terms = await Promise.all(promises)
+  terms = [].concat(...terms);
 
-	terms = [].concat(...terms)
+  promises = [];
 
-	promises = []
+  terms.forEach((term) => {
+    promises.push(mkdirp(path.join(PUBLIC_DIR, 'getTermDump', term.host)));
+    promises.push(mkdirp(path.join(PUBLIC_DIR, 'getSearchIndex', term.host)));
+  });
 
-	terms.forEach(function (term) {
-		promises.push(mkdirp(path.join(PUBLIC_DIR, 'getTermDump', term.host)))
-		promises.push(mkdirp(path.join(PUBLIC_DIR, 'getSearchIndex', term.host)))
-	})
-
-	await Promise.all(promises)
-
-
-	promises = []
-
-	terms.forEach(function (term) {
-
-		var termDumpPromises = []
-
-		var termDump = {
-			classMap: {},
-			sectionMap: {},
-			subjectMap: {},
-			termId: term.termId,
-			host: term.host
-		}
-
-		termDumpPromises.push(fireRequest('/listClasses/' + term.host + '/' + term.termId, {}, "GET").then(function (response) {
-
-			// Make a map of the hash to the classes
-			response.forEach(function (aClass) {
-				termDump.classMap[getHash(aClass)] = aClass;
-			})
-		}))
+  await Promise.all(promises);
 
 
-		termDumpPromises.push(fireRequest('/listSections/' + term.host + '/' + term.termId, {}, "GET").then(function (response) {
+  promises = [];
 
-			// Make a map of the hash to the sections
-			response.forEach(function (section) {
-				termDump.sectionMap[getHash(section)] = section;
-			})
-		}))
+  terms.forEach((term) => {
+    const termDumpPromises = [];
 
+    const termDump = {
+      classMap: {},
+      sectionMap: {},
+      subjectMap: {},
+      termId: term.termId,
+      host: term.host,
+    };
 
-		termDumpPromises.push(fireRequest('/listSubjects/' + term.host + '/' + term.termId, {}, "GET").then(function (response) {
-
-			// Make a map of the hash to the subjects
-			response.forEach(function (subject) {
-				termDump.subjectMap[getHash(subject)] = subject;
-			})
-		}))
-
-		// Download the search indexies and put them in their own dump
-		promises.push(fireRequest('/getSearchIndex/' + term.host + '/' + term.termId, {}, "GET").then(function (response) {
-			fs.writeFile(path.join(PUBLIC_DIR, 'getSearchIndex', term.host, term.termId), JSON.stringify(response))
-		}))
+    termDumpPromises.push(fireRequest(`/listClasses/${term.host}/${term.termId}`, {}, 'GET').then((response) => {
+      // Make a map of the hash to the classes
+      response.forEach((aClass) => {
+        termDump.classMap[getHash(aClass)] = aClass;
+      });
+    }));
 
 
-		// Wait for all the term dump promises and then put them in a different file
-		promises.push(Promise.all(termDumpPromises).then(function () {
-			fs.writeFile(path.join(PUBLIC_DIR, 'getTermDump', term.host, term.termId), JSON.stringify(termDump))
-		}))
-	})
+    termDumpPromises.push(fireRequest(`/listSections/${term.host}/${term.termId}`, {}, 'GET').then((response) => {
+      // Make a map of the hash to the sections
+      response.forEach((section) => {
+        termDump.sectionMap[getHash(section)] = section;
+      });
+    }));
 
 
+    termDumpPromises.push(fireRequest(`/listSubjects/${term.host}/${term.termId}`, {}, 'GET').then((response) => {
+      // Make a map of the hash to the subjects
+      response.forEach((subject) => {
+        termDump.subjectMap[getHash(subject)] = subject;
+      });
+    }));
+
+    // Download the search indexies and put them in their own dump
+    promises.push(fireRequest(`/getSearchIndex/${term.host}/${term.termId}`, {}, 'GET').then((response) => {
+      fs.writeFile(path.join(PUBLIC_DIR, 'getSearchIndex', term.host, term.termId), JSON.stringify(response));
+    }));
 
 
-	await Promise.all(promises)
-	console.log('All Done.')
+    // Wait for all the term dump promises and then put them in a different file
+    promises.push(Promise.all(termDumpPromises).then(() => {
+      fs.writeFile(path.join(PUBLIC_DIR, 'getTermDump', term.host, term.termId), JSON.stringify(termDump));
+    }));
+  });
 
+
+  await Promise.all(promises);
+  console.log('All Done.');
 }
 
-main()
+main();
