518546b2d6c5ca2928e752d313fab6b596d9c913
Ryan Hughes <ryanhughes624@gmail.com>
2017-03-06 00:56:01 -0800

    ayy lmao eslint.

--- frontend/components/macros.js
+++ frontend/components/macros.js
@@ -1,8 +1,8 @@
 // XXX: This is stuff that is hardcoded for now, need to change when expanding to other schools.
 export default {
-	collegeName: 'Northeastern University'
-}
+  collegeName: 'Northeastern University',
+};
 
-window.elog = function () {
-	console.error.apply(console.error, arguments)
-}
+window.elog = function elog() {
+  console.error.apply(console.error, arguments);
+};
