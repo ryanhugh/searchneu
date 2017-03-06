1cf0bb5f972af377dd645f59cd6cdcf4b04f51ca
Ryan Hughes <ryanhughes624@gmail.com>
2017-03-06 00:25:39 -0800

    comments and code cleaning.

--- scrapers/combineCCISandEmployees.js
+++ scrapers/combineCCISandEmployees.js
@@ -1,19 +1,25 @@
 import removeAccents from 'remove-accents';
+import mkdirp from 'mkdirp-promise';
 
-import ccisFaculty from './neuCCISFaculty'
-import neuEmployees from './neuEmployees'
+import fs from 'fs-promise';
+import path from 'path';
+import macros from './macros';
+import ccisFaculty from './neuCCISFaculty';
+import neuEmployees from './neuEmployees';
 
+// This file combines the data from the ccis website and the NEU Employees site
+// If there is a match, the data from the ccis site has priority over the data from the employee site.
+// Matching is first done by email (which is scraped from both sites) and then by name
+// Email is a great way to match people, but Name is not because some people use nicknames
 
-// plan for merging the objects with the employee database
-// 1. compare phone numbers. if any match, merge objects and remove from original lists
-// 2. compare emails. if any match, merge objects and remove from original lists
-// 3. if email.endswith husky.neu.edu in ccis site don't bother comparing
-// 4. compare names and position...
-
-
-// test changing northeastern.edu <-> neu.edu
-
+// TODO:
+// the only name on the output object is the ccis name if there was a match and the employee name if there was not
+// Could keep both, or keep the output of the first name/last name logic
+// Or maybe if keeping employee name put the first name first so it is in the same order as the ccis name
 // Names on the output from this file are "bob smith" and not "smith, bob", even if there was no match
+
+// Possible checks:
+// How often people have conflicting data field when merging (eg different phone numbers)
 
 
 // http://stackoverflow.com/questions/4009756/how-to-count-string-occurrence-in-string/7924240#7924240
@@ -21,8 +27,8 @@ function occurrences(string, subString, allowOverlapping) {
   string += '';
   subString += '';
   if (subString.length <= 0) {
-    return (string.length + 1)
-  };
+    return (string.length + 1);
+  }
 
   let n = 0;
   let pos = 0;
@@ -33,8 +39,7 @@ function occurrences(string, subString, allowOverlapping) {
     if (pos >= 0) {
       ++n;
       pos += step;
-    }
-    else {
+    } else {
       break;
     }
   }
@@ -47,7 +52,7 @@ function wlog(...args) {
   console.log.apply(console.log, arr);
 }
 
-var couldNotFindNameList = {}
+const couldNotFindNameList = {};
 
 // Given a list of things, will find the first one that is longer than 1 letter (a-z)
 function findName(list) {
@@ -60,21 +65,21 @@ function findName(list) {
   }
 
 
-  // Only log each warning once, just to not spam the console. This method is called a lot. 
-  var logMatchString = list.join('')
+  // Only log each warning once, just to not spam the console. This method is called a lot.
+  const logMatchString = list.join('');
   if (couldNotFindNameList[logMatchString]) {
-    return;
+    return null;
   }
-  couldNotFindNameList[logMatchString] = true
+  couldNotFindNameList[logMatchString] = true;
 
   console.log('Could not find name from list:', list);
   return null;
 }
 
 function getFirstLastName(employeeObj) {
-  var retVal = {}
+  const retVal = {};
 
-  var name = employeeObj.name
+  let name = employeeObj.name;
 
   if (name.match(/jr.?,/gi)) {
     name = name.replace(/, jr.?,/gi, ',');
@@ -82,7 +87,7 @@ function getFirstLastName(employeeObj) {
 
   if (occurrences(name, ',') !== 1) {
     wlog('Name has != commas', name);
-    return;
+    return null;
   }
 
   const splitOnComma = name.split(',');
@@ -99,70 +104,64 @@ function getFirstLastName(employeeObj) {
 }
 
 function mergePeople(ccisProf, employee) {
-  console.log('going to merge ', ccisProf.name, 'and ', employee.name)
+  console.log('going to merge ', ccisProf.name, 'and ', employee.name);
 
-  var output = {}
+  const output = {};
 
-  console.log(ccisProf, employee)
+  // Name is taken from the ccis profile because it is better data source.
+  // For people with multiple middle names and such, it seems to include just the importiant ones.
 
+  Object.assign(output, employee, ccisProf);
 
-  // Name is taken from the ccis profile because it is better data source. 
-  // For people with multiple middle names and such, it seems to include just the importiant ones. 
-
-  Object.assign(output, employee, ccisProf)
-
-  console.log(output)
-
-
-  process.exit()
-
+  return output;
 }
 
 function findMatchByName(employees, ccisProf) {
   for (const employee of employees) {
     const ccisCompareName = removeAccents(ccisProf.name);
 
-    var {
+    const {
       firstName,
-      lastName
-    } = getFirstLastName(employee)
+      lastName,
+    } = getFirstLastName(employee);
 
     // It would be better to split each name into first name and last name
     // And compare those individually
     // But a good chunk of names would fail if we did that instead of just a .includes
     // eg. going to merge  [Panagiotos (Pete) Manolios](ccis) and  [Manolios, Pete](employee)
     if (ccisCompareName.includes(firstName) && ccisCompareName.includes(lastName)) {
-      return employee
+      return employee;
     }
   }
-  return null
+  return null;
 }
 
 
 async function main() {
+  const ccis = await ccisFaculty.go();
+  const employees = await neuEmployees.go();
 
-  var promises = []
-
-  var ccis = await ccisFaculty.go()
-  var employees = await neuEmployees.go()
-
-  const mergedData = {}
   const emailMap = {};
+
   employees.forEach((employee) => {
-    if (employee.email) {
-      if (employee.email.endsWith('@neu.edu')) {
-        employee.email = `${employee.email.split('@')[0]}@northeastern.edu`;
+    if (employee.email && employee.email !== 'Not Available') {
+      if (emailMap[employee.email]) {
+        console.log('two employees had same email??', employee.email);
       }
 
       emailMap[employee.email] = employee;
     }
   });
 
+  const matchedPeople = [];
+
+  // Match by email. Keep track of the employees and ccis people that were not matched.
   const unmatchedProfs = [];
+  const unmatchedEmployes = [];
+  let matchedEmails = {};
 
   ccis.forEach((prof) => {
-
-    // For some reason, some different people can have the same phone number. Cannot match by phone number
+    // For some reason, some different people across the different data sources can have the same phone number. Cannot match by phone number
 
     if (prof.email && prof.email.endsWith('@neu.edu')) {
       prof.email = `${prof.email.split('@')[0]}@northeastern.edu`;
@@ -170,48 +169,61 @@ async function main() {
 
 
     if (prof.email && emailMap[prof.email]) {
-      mergePeople(prof, emailMap[prof.email])
+      matchedEmails[prof.email] = true;
+      matchedPeople.push(mergePeople(prof, emailMap[prof.email]));
       return;
     }
 
     unmatchedProfs.push(prof);
   });
 
-  console.log('halfway done')
+  employees.forEach((employee) => {
+    if (!employee.email || !matchedEmails[employee.email]) {
+      unmatchedEmployes.push(employee);
+    }
+  });
 
 
+  console.log('Now matching by name');
+
+  // Now try to match by name
+  matchedEmails = {};
   const finalUnmatchedProfs = [];
 
   for (const ccisProf of unmatchedProfs) {
-    let found = false;
-
-
-    var employee = findMatchByName(employees, ccisProf)
+    const employee = findMatchByName(unmatchedEmployes, ccisProf);
 
     if (employee) {
-      mergePeople(ccisProf, employee)
-    }
-    else {
-      finalUnmatchedProfs.push(ccisProf)
+      matchedPeople.push(mergePeople(ccisProf, employee));
+      const index = unmatchedEmployes.indexOf(employee);
+      unmatchedEmployes.splice(index, 1);
+    } else {
+      finalUnmatchedProfs.push(ccisProf);
     }
   }
 
 
   let phdStudentCount = 0;
 
-  finalUnmatchedProfs.forEach((a) => {
-    if (a.positions && a.positions.length === 1 && a.positions[0] === 'PhD Student') {
+  finalUnmatchedProfs.forEach((prof) => {
+    if (prof.positions && prof.positions.length === 1 && prof.positions[0] === 'PhD Student') {
       phdStudentCount++;
-      return;
     }
-
   });
 
-  console.log('Unable to match ', finalUnmatchedProfs.length, '/', ccis.length)
+  console.log('Unable to match ', finalUnmatchedProfs.length, '/', ccis.length);
+  console.log(phdStudentCount, 'of the unmatched people are PhD students who are usually not in the employee directory.');
 
 
-  console.log(finalUnmatchedProfs.length, phdStudentCount);
+  const output = unmatchedEmployes.concat(finalUnmatchedProfs).concat(matchedPeople);
 
+  console.log(output.length, employees.length, ccis.length);
+
+  await mkdirp(macros.PUBLIC_DIR);
+
+  await fs.writeFile(path.join(macros.PUBLIC_DIR, 'employees.json'), JSON.stringify(output));
+
+  return output;
 }
 
 
