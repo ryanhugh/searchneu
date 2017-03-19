f0a707ca435865615d5b2daca2ce458243dd49aa
Ryan Hughes <ryanhughes624@gmail.com>
2017-03-19 00:40:30 -0700

    .

--- scrapers/neuCOEFaculty.js
+++ scrapers/neuCOEFaculty.js
@@ -4,129 +4,170 @@ import fs from 'fs-promise';
 import mkdirp from 'mkdirp-promise';
 import path from 'path';
 import URI from 'urijs';
-import Throttle from 'superagent-throttle'
+import Throttle from 'superagent-throttle';
 
+import utils from './utils';
 import macros from './macros';
 
+
+// TODO
+
+// Could parse a lot more from each page
+// Phone numbers with extentions are not parsed http://www.civ.neu.edu/people/patterson-mark
+
+
+
+
+let throttle = new Throttle({
+  active: true,     // set false to pause queue
+  rate: 20,          // how many requests can be sent every `ratePer`
+  ratePer: 10000,   // number of ms in which `rate` requests may be sent
+  concurrent: 5     // how many requests can be sent concurrently
+})
+.on('sent', (request) => { console.log('sent') }) // sent a request
+.on('received', (request) => { console.log('received') }) // received a response
+.on('drained', () => { console.log('drained') }) // received last response
 
 
 
 
 // http://www.coe.neu.edu/connect/directory?field_faculty_type_value=faculty&letter=A
-// 
 
 
 async function scrapeDetailpage(obj) {
-    
-  const resp = await request.post(`http://www.ece.neu.edu/people/erdogmus-deniz`);
+  const resp = await request.get('http://www.ece.neu.edu/people/erdogmus-deniz');
 
   const $ = cheerio.load(resp.text);
-  debugger
+  debugger;
 
   // full resolution image
-  $('#faculty-profile > div.upper-content > div > div.left-content > a').attr('href')
+  $('#faculty-profile > div.upper-content > div > div.left-content > a').attr('href');
 
 
   // linkedin link
-  $('div.field-name-field-nucoe-social-link-url > div > div > a.linkedin').attr('href')
+  $('div.field-name-field-nucoe-social-link-url > div > div > a.linkedin').attr('href');
 
-  $('div.field-name-field-nucoe-social-link-url > div > div > a.googlescholar').attr('href')
+  $('div.field-name-field-nucoe-social-link-url > div > div > a.googlescholar').attr('href');
 
-  
-  $('div.field-name-field-nucoe-social-link-url > div > div > a.youtube').attr('href')
+
+  $('div.field-name-field-nucoe-social-link-url > div > div > a.youtube').attr('href');
 
   // example of person who has multiple roles in departments
   // http://www.che.neu.edu/people/ebong-eno
   // Position and department
-  $('div.field-collection-container > div.faculty-roles > div.faculty-roles__role',$(items[0]))
+  $('div.field-collection-container > div.faculty-roles > div.faculty-roles__role', $(items[0]));
 
   // position (includes trailing comma)
-  $('div.field-collection-container > div.faculty-roles > div.faculty-roles__role',$(items[0]))[0].children[0].data
+  $('div.field-collection-container > div.faculty-roles > div.faculty-roles__role', $(items[0]))[0].children[0].data;
 
 
   // department
-    $('div.field-collection-container > div.faculty-roles > div.faculty-roles__role > a',$(items[0])).text()
+  $('div.field-collection-container > div.faculty-roles > div.faculty-roles__role > a', $(items[0])).text();
 
 // address
-    $('div.faculty-profile__address').text().trim().replace(/[\n\r]+\s*/gi,'\n')
+  $('div.faculty-profile__address').text().trim().replace(/[\n\r]+\s*/gi, '\n');
 
     // might be more than one of these, need to check .text() for each one
-    // if text matches Faculty Website then get href 
+    // if text matches Faculty Website then get href
     // also need to do head checks or get checks to make sure their site is up
-    $($('div.field-name-field-faculty-links a')[0]).text()
+  $($('div.field-name-field-faculty-links a')[0]).text();
 
 
-
-  debugger
-
-
+  debugger;
 }
-
-
 
 
 async function scrapeLetter(letter) {
-  const resp = await request.post(`http://www.coe.neu.edu/connect/directory?field_faculty_type_value=faculty&letter=${letter.toUpperCase()}`);
+  const resp = await request.get(`http://www.coe.neu.edu/connect/directory?field_faculty_type_value=faculty&letter=${letter.toUpperCase()}`).use(throttle.plugin());
 
   const $ = cheerio.load(resp.text);
 
+  const peopleElements = $('div.item-list > ul > li.views-row');
 
-  items =$('div.item-list > ul > li.views-row')
+  const people = [];
+  for (let i = 0; i < peopleElements.length; i++) {
+    const personElement = peopleElements[i];
 
-  // thumbnail image of them
-  $('h4 > a > img',$(items[0])).attr('src')
+    const $personElement = $(personElement);
 
-  // link to their page
-  $('h4 > a',$(items[0])).attr('href')
+    var obj = {};
 
-  // name of prof
-  $('div.views-field.views-field-field-faculty-last-name > h4 > a',$(items[0])).text()
+    // thumbnail image of them
+    obj.picThumbnail = $('h4 > a > img', $personElement).attr('src');
 
+    // link to their page
+    obj.link = $('h4 > a', $personElement).attr('href');
 
+    // name of prof
+    obj.name = $('div.views-field.views-field-field-faculty-last-name > h4 > a', $personElement).text().trim();
 
     // interests
-      $('div.field-name-field-faculty-interests',$(items[0])).text()
+    obj.interests = $('div.field-name-field-faculty-interests', $personElement).text().trim();
 
-      // email, inncludes mailto: in the beginning
-        $('div.views-field-field-faculty-email > div.field-content > a',$(items[0])).attr('href')
+    // Parse email
+    let email = $('div.views-field-field-faculty-email > div.field-content > a', $personElement).attr('href');
 
-        // phone. need to remove all non digits and then check length
-          $('div.views-field-field-faculty-phone > div.field-content',$(items[0])).text()
+    email = utils.standardizeEmail(email);
 
-  debugger
+    if (!email) {
+      console.log('Could not parse email');
+    }
+
+    obj.email = email;
+
+    // Phone
+    let phone = $('div.views-field-field-faculty-phone > div.field-content', $personElement).text();
+
+    phone = utils.standardizePhone(phone);
+
+    if (phone) {
+      obj.phone = phone;
+    }
 
 
+    ['picThumbnail', 'link', 'name', 'interests', 'email', 'phone'].forEach((attr) => {
+      if (!obj[attr]) {
+        console.log('obj missing ', attr, obj.name);
+      }
+    });
 
+    people.push(obj)
+  }
 
-
-
+  return people;
 }
-
 
 
 async function main() {
 
-  scrapeDetailpage()
-  return;
+  const outputFile = path.join(macros.DEV_DATA_DIR, 'coe.json')
 
 
-  scrapeLetter('a')
-  return;
-
-  var promises = []
+  const promises = [];
+  let people = [];
 
 
 
   macros.ALPHABET.split('').forEach((letter) => {
-    promises.push(scrapeLetter(letter))
+    promises.push(scrapeLetter(letter).then((peopleFromLetter) => {
+      people = people.concat(peopleFromLetter);
+    }));
+  });
 
-  })
+  await Promise.all(promises);
+
+  console.log(people.length);
 
 
+  if (macros.DEV) {
+    await fs.writeFile(outputFile, JSON.stringify(people));
+    console.log('saved file')
+  }
+  return people
 }
 
 
-exports.go = main
+exports.go = main;
 
 if (require.main === module) {
   main();
---
+++ scrapers/utils.js
@@ -0,0 +1,46 @@
+
+// Need to abstract the retry and throttle logic too
+// max number of simultaneous requests
+// auto retry with linear backoff
+// application layer dns caching
+// keep-alive connections (forever: true in request)
+// ignore invalid https certs (rejectUnauthorized: false) and outdated ciphers (ciphers: 'ALL')
+  
+      
+
+
+
+
+exports.standardizeEmail = function standardizeEmail(email) {
+  if (email.startsWith('mailto:')) {
+    email = email.slice('mailto:'.length);
+  }
+
+  if (!email.includes('@')) {
+  	return null
+  }
+
+  return email;
+};
+
+
+exports.standardizePhone = function standardizePhone(phone) {
+  phone = phone.trim()
+
+  if (phone.startsWith('tel:')) {
+    phone = phone.slice('tel:').trim()
+  }
+
+  let digitsOnly = phone.replace(/[^0-9]/gi, '');
+
+
+  if (phone.startsWith('+1') && digitsOnly.length === 11) {
+    digitsOnly = digitsOnly.slice(1)
+  }
+
+  if (digitsOnly.length != 10) {
+  	return null
+  }
+
+  return digitsOnly;
+};
