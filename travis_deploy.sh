3b11ec79b0a1681bbead854280601025c508c1f1
Ryan Hughes <ryanhughes624@gmail.com>
2017-04-02 02:50:35 -0700

    .

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -10,20 +10,23 @@ fi
 rm test
 
 eval "$(ssh-agent -s)"
-echo $GIT_PRIVATE_KEY | base64 --decode > ~/deploy_key
-chmod 600 ~/deploy_key
-ssh-add ~/deploy_key
-git remote remove origin
-git remote add origin git@github.com:ryanhugh/neusearch.git
-git pull origin master
-git pull origin gh-pages
-git checkout gh-pages
-git config --global user.email "ryanhughes624+gitbot@gmail.com"
-git config --global user.name "data-updater-bot"
-git merge master --no-edit
-git commit -a -m "Merged the latest changes into gh-pages"
-# node main.js	
-# git commit -a -m "Updated data"
-git remote add deploy git@github.com:ryanhugh/neusearch.git
-git config --global push.default simple
-git push git@github.com:ryanhugh/neusearch.git gh-pages
+mkdir ~/.ssh 2> /dev/null
+echo $GIT_PRIVATE_KEY | base64 --decode > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_rsa
+node node_modules/gh-pages/bin/gh-pages -d public
+
+# git remote remove origin
+# git remote add origin git@github.com:ryanhugh/neusearch.git
+# git pull origin master
+# git pull origin gh-pages
+# git checkout gh-pages
+# git config --global user.email "ryanhughes624+gitbot@gmail.com"
+# git config --global user.name "data-updater-bot"
+# git merge master --no-edit
+# git commit -a -m "Merged the latest changes into gh-pages"
+# # node main.js	
+# # git commit -a -m "Updated data"
+# git remote add deploy git@github.com:ryanhugh/neusearch.git
+# git config --global push.default simple
+# git push git@github.com:ryanhugh/neusearch.git gh-pages
