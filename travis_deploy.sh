c162ac9a0b9b8b3fba7ec7695d4065a35d605351
Ryan Hughes <ryanhughes624@gmail.com>
2017-04-30 23:34:39 -0400

    added rollbar to the deploy

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -19,3 +19,14 @@ git remote remove origin
 git remote add origin git@github.com:ryanhugh/searchneu.git
 node node_modules/gh-pages/bin/gh-pages -d public -a
 
+# Tell Rollbar about the deploy
+ACCESS_TOKEN=$ROLLBAR_TOKEN
+ENVIRONMENT=production
+LOCAL_USERNAME=`whoami`
+REVISION=`git log -n 1 --pretty=format:"%H"`
+
+curl https://api.rollbar.com/api/1/deploy/ \
+  -F access_token=$ACCESS_TOKEN \
+  -F environment=$ENVIRONMENT \
+  -F revision=$REVISION \
+  -F local_username=$LOCAL_USERNAME
