59a4ed3d38603e34f76f6519cb209eec53454c96
Ryan Hughes <ryanhughes624@gmail.com>
2017-05-14 22:18:56 -0700

    debugging gh pages

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -42,7 +42,7 @@ git config --global user.email "ryanhughes624+gitbot@gmail.com"
 git config --global user.name "data-updater-bot"
 git remote remove origin
 git remote add origin git@github.com:ryanhugh/searchneu.git
-node node_modules/gh-pages/bin/gh-pages -d public -a
+NODE_DEBUG=gh-pages node node_modules/gh-pages/bin/gh-pages -d public -a
 echo $? 
 
 # Tell Rollbar about the deploy
