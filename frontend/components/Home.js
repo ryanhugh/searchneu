eaddd1fcc5fbf54fee13da0705d1176edb32b777
Ryan Hughes <ryanhughes624@gmail.com>
2017-05-30 21:36:17 -0700

    eslint home.js

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -2,16 +2,13 @@ import React from 'react';
 import CSSModules from 'react-css-modules';
 import 'semantic-ui-css/semantic.min.css';
 import ReactTooltip from 'react-tooltip';
-import URI from 'urijs';
 
 import '../css/base.css';
 
-import search from './search'
+import search from './search';
 import css from './home.css';
 import macros from './macros';
-import request from './request';
 import ResultsLoader from './ResultsLoader';
-import CourseProData from '../../common/classModels/DataLib';
 
 
 // Home page component
@@ -55,10 +52,10 @@ class Home extends React.Component {
   // Replace all instances of a substring with another without a regex (faster).
   // https://stackoverflow.com/questions/16803931/replace-all-without-a-regex-where-can-i-use-the-g
   replaceAll(string, old, newString) {
-    var index = 0;
+    let index = 0;
     do {
-        string = string.replace(old, newString);
-    } while((index = string.indexOf(old, index + 1)) > -1);
+      string = string.replace(old, newString);
+    } while ((index = string.indexOf(old, index + 1)) > -1);
     return string;
   }
 
@@ -74,15 +71,14 @@ class Home extends React.Component {
     console.log('Logging', searchTerm);
 
     if (searchTerm) {
-      ga('send', 'pageview', `/?search=${searchTerm}`);
+      window.ga('send', 'pageview', `/?search=${searchTerm}`);
     } else {
-      ga('send', 'pageview', '/');
+      window.ga('send', 'pageview', '/');
     }
   }
 
 
   async componentDidMount() {
-
     if (this.state.searchTerm) {
       console.log('Going to serach for', this.state.searchTerm);
       this.search(this.state.searchTerm);
@@ -92,25 +88,23 @@ class Home extends React.Component {
     // (This is just for testing, feel free to change it to whatever.)
     else if (macros.DEV) {
       this.search('cs');
-    }
-    else {
-
-      // Force an update on the screen so the loading bar dissapears and the page shows. 
+    } else {
+      // Force an update on the screen so the loading bar dissapears and the page shows.
       this.forceUpdate();
     }
   }
 
   // Called from ResultsLoader to load more
   loadMore() {
-    this.search(this.state.searchTerm, this.state.results.length + 10)
+    this.search(this.state.searchTerm, this.state.results.length + 10);
   }
 
   async search(searchTerm, termCount = 4) {
     this.currentQuery = searchTerm;
-    let results = await search.search(searchTerm, termCount)
-    
+    const results = await search.search(searchTerm, termCount);
+
     if (searchTerm !== this.currentQuery) {
-      console.log("Did not come back in order, discarding ", searchTerm);
+      console.log('Did not come back in order, discarding ', searchTerm);
       return;
     }
 
@@ -200,7 +194,7 @@ class Home extends React.Component {
       } else {
         resultsElement = (<ResultsLoader
           results={ this.state.results }
-          loadMore = { this.loadMore }
+          loadMore={ this.loadMore }
         />);
       }
     }
@@ -218,11 +212,13 @@ class Home extends React.Component {
       <div>
 
         <a href='https://github.com/ryanhugh/neusearch' className='github-corner'>
+          {/* eslint-disable max-len */}
           <svg width='80' height='80' viewBox='0 0 250 250' className={ css.githubCornerIcon } >
             <path d='M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z' />
             <path d='M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2' fill='currentColor' className={ `${css.octopusArm} octo-arm` } />
-            <path d='M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z' fill='currentColor' className='octo-body' />
+            <path d='M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z' fill='currentColor' className='octo-body' /> {/* eslint-disable max-len*/}
           </svg>
+          {/* eslint-enable max-len */}
         </a>
 
         <div className={ css.topPadding } />
