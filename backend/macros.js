831e1f27b23318b8ec44334755be09bc1a91b4a3
Ryan Hughes <ryanhughes624@gmail.com>
2017-07-02 23:19:10 -0700

    Now using super and actual static methods for macros, and logging works.

--- backend/macros.js
+++ backend/macros.js
@@ -28,7 +28,210 @@ while (1) {
 
 class Macros extends commonMacros {
 
+
+  static parseNameWithSpaces(name) {
+    // Standardize spaces.
+    name = name.replace(/\s+/gi, ' ');
+
+    // Generate first name and last name
+    const spaceCount = Macros.occurrences(name, ' ', false);
+    const splitName = name.split(' ');
+
+
+    if (spaceCount === 0) {
+      Macros.critical('0 spaces found in name', name);
+      return null;
+    }
+
+    // Handles firstName, lastName and firstName, middleName, lastName
+
+    if (spaceCount > 2) {
+      Macros.log(name, 'has more than 1 space in their name. Using first and last word.');
+    }
+
+    const obj = {};
+
+    obj.firstName = splitName[0];
+    obj.lastName = splitName[splitName.length - 1];
+
+    return obj;
+  };
+
+  // Standardizes email addresses found across different pages
+  // Removes a 'mailto:' from the beginning
+  // Ensures the email contains a @
+  static standardizeEmail(email) {
+    if (email.startsWith('mailto:')) {
+      email = email.slice('mailto:'.length);
+    }
+
+    if (!email.includes('@') || email.includes(' ')) {
+      return null;
+    }
+
+    if (email.endsWith('@neu.edu')) {
+      email = `${email.split('@')[0]}@northeastern.edu`;
+    }
+
+    return email.toLowerCase();
+  };
+
+
+  static standardizePhone(phone) {
+    phone = phone.trim();
+
+    if (phone.startsWith('tel:')) {
+      phone = phone.slice('tel:'.length).trim();
+    }
+
+    let digitsOnly = phone.replace(/[^0-9]/gi, '');
+
+
+    if (phone.startsWith('+1') && digitsOnly.length === 11) {
+      digitsOnly = digitsOnly.slice(1);
+    }
+
+    if (digitsOnly.length !== 10) {
+      return null;
+    }
+
+    return digitsOnly;
+  };
+
+  // Parses the google scholar id from a link that should contain a google scholar link.
+  // Get the Google Scholar ID with this: https://scholar.google.com/citations?user=[id here]
+  static parseGoogleScolarLink(link) {
+    if (!link) {
+      return null;
+    }
+
+    const userId = new URI(link).query(true).user;
+    if (!userId && link) {
+      Macros.log('Error parsing google url', link);
+      return null;
+    }
+    return userId;
+  };
+
+
+  // Gets the base hostname from a url.
+  // fafjl.google.com -> google.com
+  // subdomain.bob.co -> bob.co
+  // bob.co -> bob.co
+  // This could be improved by using public lists of top-level domains. 
+  static getBaseHost(url) {
+    const homepage = new URI(url).hostname();
+    if (!homepage || homepage === '') {
+      Macros.error('could not find homepage of', url);
+      return null;
+    }
+
+    const match = homepage.match(/[^.]+\.[^.]+$/i);
+    if (!match) {
+      Macros.error('homepage match failed...', homepage);
+      return null;
+    }
+    return match[0];
+  }
+
+
+
+  // http://stackoverflow.com/questions/4009756/how-to-count-string-occurrence-in-string/7924240#7924240
+  static occurrences(string, subString, allowOverlapping) {
+    string += '';
+    subString += '';
+    if (subString.length <= 0) {
+      return (string.length + 1);
+    }
+
+    let n = 0;
+    let pos = 0;
+    const step = allowOverlapping ? 1 : subString.length;
+
+    while (true) {
+      pos = string.indexOf(subString, pos);
+      if (pos >= 0) {
+        ++n;
+        pos += step;
+      } else {
+        break;
+      }
+    }
+    return n;
+  };
+
+  // Loads dev data from the given file path. 
+  // If you want to use this, just see how it is used in other places and copy that. 
+  static async loadDevData(path) {
+    if (!Macros.DEV) {
+      Macros.error('Called load dev data while not in dev mode.');
+      return null;
+    }
+
+    await mkdirp(Macros.DEV_DATA_DIR);
+    const exists = await fs.exists(path);
+    if (exists) {
+      return JSON.parse(await fs.readFile(path));
+    }
+
+    return null;
+  };
+
+  // Inverse of loadDevData. 
+  static async saveDevData(path, data) {
+    if (!Macros.DEV) {
+      Macros.error('Called save dev data while not in dev mode.');
+      return;
+    }
+
+    await mkdirp(Macros.DEV_DATA_DIR);
+    await fs.writeFile(path, JSON.stringify(data, null, 4));
+  };
+
+
+  // This is for programming errors. This will cause the program to exit anywhere.
+  // This *should* never be called.
+  static critical(...args) {
+    Macros.error.apply(Macros.error, args);
+    process.exit(1);
+  };
+
+  // Use this for stuff that should never happen, but does not mean the program cannot continue.
+  // This will continue running in dev, but will exit on CI
+  // Will log stack trace
+  // and cause CI to fail
+  // so CI will send an email
+  static error(...args) {
+    super.error(...args);
+   
+    // So I get an email about it
+    if (process.env.CI) {
+      process.exit(1);
+    }
+  };
+
+  // Use console.warn to log stuff during testing
+
+  // Use this for normal logging
+  // Will log as normal, but stays silent during testing
+  static log(...args) {
+    if (process.env.NODE_ENV === 'test') {
+      return;
+    }
+
+    console.log.apply(console.log, args);
+  };
+
+  static verbose(...args) {
+    if (!process.env.VERBOSE) {
+      return;
+    }
+
+    console.log.apply(console.log, args);
+  };
+
 }
+
 
 Macros.PUBLIC_DIR = path.join('public', 'data');
 Macros.DEV_DATA_DIR = path.join('cache', 'dev_data');
@@ -36,212 +239,6 @@ Macros.DEV_DATA_DIR = path.join('cache', 'dev_data');
 // For iterating over every letter in a couple different places in the code.
 Macros.ALPHABET = 'maqwertyuiopsdfghjklzxcvbn';
 
-
-Macros.parseNameWithSpaces = function parseNameWithSpaces(name) {
-  // Standardize spaces.
-  name = name.replace(/\s+/gi, ' ');
-
-  // Generate first name and last name
-  const spaceCount = Macros.occurrences(name, ' ', false);
-  const splitName = name.split(' ');
-
-
-  if (spaceCount === 0) {
-    Macros.critical('0 spaces found in name', name);
-    return null;
-  }
-
-  // Handles firstName, lastName and firstName, middleName, lastName
-
-  if (spaceCount > 2) {
-    Macros.log(name, 'has more than 1 space in their name. Using first and last word.');
-  }
-
-  const obj = {};
-
-  obj.firstName = splitName[0];
-  obj.lastName = splitName[splitName.length - 1];
-
-  return obj;
-};
-
-// Standardizes email addresses found across different pages
-// Removes a 'mailto:' from the beginning
-// Ensures the email contains a @
-Macros.standardizeEmail = function standardizeEmail(email) {
-  if (email.startsWith('mailto:')) {
-    email = email.slice('mailto:'.length);
-  }
-
-  if (!email.includes('@') || email.includes(' ')) {
-    return null;
-  }
-
-  if (email.endsWith('@neu.edu')) {
-    email = `${email.split('@')[0]}@northeastern.edu`;
-  }
-
-  return email.toLowerCase();
-};
-
-
-Macros.standardizePhone = function standardizePhone(phone) {
-  phone = phone.trim();
-
-  if (phone.startsWith('tel:')) {
-    phone = phone.slice('tel:'.length).trim();
-  }
-
-  let digitsOnly = phone.replace(/[^0-9]/gi, '');
-
-
-  if (phone.startsWith('+1') && digitsOnly.length === 11) {
-    digitsOnly = digitsOnly.slice(1);
-  }
-
-  if (digitsOnly.length !== 10) {
-    return null;
-  }
-
-  return digitsOnly;
-};
-
-// Parses the google scholar id from a link that should contain a google scholar link.
-// Get the Google Scholar ID with this: https://scholar.google.com/citations?user=[id here]
-Macros.parseGoogleScolarLink = function parseGoogleScolarLink(link) {
-  if (!link) {
-    return null;
-  }
-
-  const userId = new URI(link).query(true).user;
-  if (!userId && link) {
-    Macros.log('Error parsing google url', link);
-    return null;
-  }
-  return userId;
-};
-
-
-// Gets the base hostname from a url.
-// fafjl.google.com -> google.com
-// subdomain.bob.co -> bob.co
-// bob.co -> bob.co
-// This could be improved by using public lists of top-level domains. 
-Macros.getBaseHost = function getBaseHost(url) {
-  const homepage = new URI(url).hostname();
-  if (!homepage || homepage === '') {
-    Macros.error('could not find homepage of', url);
-    return null;
-  }
-
-  const match = homepage.match(/[^.]+\.[^.]+$/i);
-  if (!match) {
-    Macros.error('homepage match failed...', homepage);
-    return null;
-  }
-  return match[0];
-}
-
-
-
-// http://stackoverflow.com/questions/4009756/how-to-count-string-occurrence-in-string/7924240#7924240
-Macros.occurrences = function occurrences(string, subString, allowOverlapping) {
-  string += '';
-  subString += '';
-  if (subString.length <= 0) {
-    return (string.length + 1);
-  }
-
-  let n = 0;
-  let pos = 0;
-  const step = allowOverlapping ? 1 : subString.length;
-
-  while (true) {
-    pos = string.indexOf(subString, pos);
-    if (pos >= 0) {
-      ++n;
-      pos += step;
-    } else {
-      break;
-    }
-  }
-  return n;
-};
-
-// Loads dev data from the given file path. 
-// If you want to use this, just see how it is used in other places and copy that. 
-Macros.loadDevData = async function loadDevData(path) {
-  if (!Macros.DEV) {
-    Macros.error('Called load dev data while not in dev mode.');
-    return null;
-  }
-
-  await mkdirp(Macros.DEV_DATA_DIR);
-  const exists = await fs.exists(path);
-  if (exists) {
-    return JSON.parse(await fs.readFile(path));
-  }
-
-  return null;
-};
-
-// Inverse of loadDevData. 
-Macros.saveDevData = async function saveDevData(path, data) {
-  if (!Macros.DEV) {
-    Macros.error('Called save dev data while not in dev mode.');
-    return;
-  }
-
-  await mkdirp(Macros.DEV_DATA_DIR);
-  await fs.writeFile(path, JSON.stringify(data, null, 4));
-};
-
-
-// This is for programming errors. This will cause the program to exit anywhere.
-// This *should* never be called.
-Macros.critical = function critical(...args) {
-  Macros.error.apply(Macros.error, args);
-  process.exit(1);
-};
-
-// Use this for stuff that should never happen, but does not mean the program cannot continue.
-// This will continue running in dev, but will exit on CI
-// Will log stack trace
-// and cause CI to fail
-// so CI will send an email
-Macros.error = function error(...args) {
-  if (process.env.NODE_ENV === 'test') {
-    return;
-  }
-
-  console.error.apply(console.error, ['Error:'].concat(args));
-  console.trace();
-
-  // So I get an email about it
-  if (process.env.CI) {
-    process.exit(1);
-  }
-};
-
-// Use console.warn to log stuff during testing
-
-// Use this for normal logging
-// Will log as normal, but stays silent during testing
-Macros.log = function log(...args) {
-  if (process.env.NODE_ENV === 'test') {
-    return;
-  }
-
-  console.log.apply(console.log, args);
-};
-
-Macros.verbose = function verbose(...args) {
-  if (!process.env.VERBOSE) {
-    return;
-  }
-
-  console.log.apply(console.log, args);
-};
 
 Macros.verbose('Starting in verbose mode.');
 
--- common/macros.js
+++ common/macros.js
@@ -6,13 +6,97 @@
 class Macros {
 
   static error(...args) {
-    if (process.env.NODE_ENV === 'test') {
+    if (Macros.TESTS) {
       return;
     }
 
     console.error.apply(console.error, ['Error:'].concat(args));
     console.trace();
+  }
 
+
+  // Strips the middle name from a name.
+  // The given full name is the person's full name, including first, middle, and last names
+  // Full name is often not equal to first + ' ' + middle + ' ' + last because many people have foreign names and nicknames.
+  // If firstName and lastName are passed, the accuracy of this function should be higher.
+  // This is used in mobile class panel view. 
+  // And also when adding professor names (both from the employee data sources and from the classes data sources) to the search index.
+  // Even if the middle names are kept, any word that is one character (not including symbols) is still removed). 
+
+  // TODO: Remove "Jr." and "III" and other titles. 
+  // https://www.npmjs.com/package/parse-full-name
+  // Might help.
+  // Also, could merge the name functions from employees.js into this. 
+  static stripMiddleName(fullName, keepIfMoreThanOneChar = false, firstName = null, lastName = null) {
+    if ((!firstName && lastName) || (firstName && !lastName)) {
+      console.error('Need either first and last name or neither first nor last name for stripeMiddleName.');
+      console.trace();
+      return null;
+    }
+
+
+    const indexOfFirstSpace = fullName.indexOf(' ');
+
+    // If there are no spaces in this name, just return the full name.
+    if (indexOfFirstSpace === -1) {
+      return fullName;
+    }
+
+
+    let nameWithoutFirstAndLastName;
+
+    if (firstName && lastName) {
+      if (!fullName.startsWith(firstName)) {
+        this.error('Full name does not start with first name?', fullName, '|', firstName);
+      }
+
+      if (!fullName.endsWith(lastName)) {
+        this.error('Full name does not end with last name?', fullName, '|', lastName);
+      }
+    }
+
+    // Find the last name and first name by splitting the name by spaces
+    else {
+      const indexOfLastSpace = fullName.length - fullName.split('').reverse().join('').indexOf(' ');
+
+      firstName = fullName.slice(0, indexOfFirstSpace);
+      lastName = fullName.slice(indexOfLastSpace);
+    }
+
+    // No need to calculate the middle name if we are going to drop in anyway. 
+    if (!keepIfMoreThanOneChar) {
+      return `${firstName} ${lastName}`
+    }
+
+
+    // If their middle name is one character (not including symbols), don't add it to the search index.
+    // This prevents profs like Stacy C. Marsella from coming up when you type in [C]
+    // First, remove the first and last names and toLowerCase()
+    nameWithoutFirstAndLastName = fullName.replace(firstName, '').replace(lastName, '');
+
+    // Then remove symbols.
+    nameWithoutFirstAndLastName = nameWithoutFirstAndLastName.replace(/[^a-z0-9]/gi, '');
+
+    // If little to nothing remains, just index the first and last names.
+    if (keepIfMoreThanOneChar && nameWithoutFirstAndLastName.length > 1) {
+
+      // Purge middle names that are only one char long
+      let fullNameSplit = fullName.split(' ')
+
+      // Of the names that remain, remove the ones that are only 1 letter long (not including symbols)
+      fullNameSplit = fullNameSplit.filter(function (word) {
+        if (word.replace(/[^a-zA-Z0-9]/gi,'').length < 2) {
+          return false;
+        }
+        else {
+          return true;
+        }
+      })
+
+      return fullNameSplit.join(' ');
+    }
+
+    return `${firstName} ${lastName}`;
   }
 
 }
@@ -43,91 +127,5 @@ if (!Macros.TESTS) {
   Macros.TESTS = false;
 }
 
-
-// Strips the middle name from a name.
-// The given full name is the person's full name, including first, middle, and last names
-// Full name is often not equal to first + ' ' + middle + ' ' + last because many people have foreign names and nicknames.
-// If firstName and lastName are passed, the accuracy of this function should be higher.
-// This is used in mobile class panel view. 
-// And also when adding professor names (both from the employee data sources and from the classes data sources) to the search index.
-// Even if the middle names are kept, any word that is one character (not including symbols) is still removed). 
-
-// TODO: Remove "Jr." and "III" and other titles. 
-// https://www.npmjs.com/package/parse-full-name
-// Might help.
-// Also, could merge the name functions from employees.js into this. 
-Macros.stripMiddleName = function(fullName, keepIfMoreThanOneChar = false, firstName = null, lastName = null) {
-  if ((!firstName && lastName) || (firstName && !lastName)) {
-    console.error('Need either first and last name or neither first nor last name for stripeMiddleName.');
-    console.trace();
-    return null;
-  }
-
-
-  const indexOfFirstSpace = fullName.indexOf(' ');
-
-  // If there are no spaces in this name, just return the full name.
-  if (indexOfFirstSpace === -1) {
-    return fullName;
-  }
-
-
-  let nameWithoutFirstAndLastName;
-
-  console.log(this)
-
-  if (firstName && lastName) {
-    if (!fullName.startsWith(firstName)) {
-      this.error('Full name does not start with first name?', fullName, '|', firstName);
-    }
-
-    if (!fullName.endsWith(lastName)) {
-      this.error('Full name does not end with last name?', fullName, '|', lastName);
-    }
-  }
-
-  // Find the last name and first name by splitting the name by spaces
-  else {
-    const indexOfLastSpace = fullName.length - fullName.split('').reverse().join('').indexOf(' ');
-
-    firstName = fullName.slice(0, indexOfFirstSpace);
-    lastName = fullName.slice(indexOfLastSpace);
-  }
-
-  // No need to calculate the middle name if we are going to drop in anyway. 
-  if (!keepIfMoreThanOneChar) {
-    return `${firstName} ${lastName}`
-  }
-
-
-  // If their middle name is one character (not including symbols), don't add it to the search index.
-  // This prevents profs like Stacy C. Marsella from coming up when you type in [C]
-  // First, remove the first and last names and toLowerCase()
-  nameWithoutFirstAndLastName = fullName.replace(firstName, '').replace(lastName, '');
-
-  // Then remove symbols.
-  nameWithoutFirstAndLastName = nameWithoutFirstAndLastName.replace(/[^a-z0-9]/gi, '');
-
-  // If little to nothing remains, just index the first and last names.
-  if (keepIfMoreThanOneChar && nameWithoutFirstAndLastName.length > 1) {
-
-    // Purge middle names that are only one char long
-    let fullNameSplit = fullName.split(' ')
-
-    // Of the names that remain, remove the ones that are only 1 letter long (not including symbols)
-    fullNameSplit = fullNameSplit.filter(function (word) {
-      if (word.replace(/[^a-zA-Z0-9]/gi,'').length < 2) {
-        return false;
-      }
-      else {
-        return true;
-      }
-    })
-
-    return fullNameSplit.join(' ');
-  }
-
-  return `${firstName} ${lastName}`;
-}
 
 export default Macros;
