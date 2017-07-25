588dd2d52f765c2afede706357c43ff1cf413140
Ryan Hughes <ryanhughes624@gmail.com>
2017-07-24 20:04:38 -0700

    eslint base parser tests

--- backend/scrapers/classes/parsers/tests/baseParser.test.js
+++ backend/scrapers/classes/parsers/tests/baseParser.test.js
@@ -13,7 +13,7 @@
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>. 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 import fs from 'fs-promise';
@@ -22,24 +22,22 @@ import request from '../../../request';
 import baseParser from '../baseParser';
 
 
-it('toTitleCase works', function () {
-
+it('toTitleCase works', () => {
   expect(baseParser.toTitleCase('TBA')).toBe('TBA');
   expect(baseParser.toTitleCase('Texas A&M University')).toBe('Texas A&M University');
 });
 
-it('standardizeClassName', function () {
-
+it('standardizeClassName', () => {
   expect(baseParser.standardizeClassName('2nd Year Japanese')).toBe('2nd Year Japanese');
   expect(baseParser.standardizeClassName('BMC: General Chemistry II')).toBe('BMC: General Chemistry II');
 
-  var goodName = 'Interactive Learning Seminar for Physics 1151'
+  let goodName = 'Interactive Learning Seminar for Physics 1151';
   expect(baseParser.standardizeClassName('Int. Learn for Phys 1151', [goodName])).toBe(goodName);
 
-  var goodName = 'Connections and Decisions'
+  goodName = 'Connections and Decisions';
   expect(baseParser.standardizeClassName('Connections & Decisions', ['hihfdsjal', goodName])).toBe(goodName);
 
-  var classNameTranslation = {
+  let classNameTranslation = {
 
     // math
     'Calculus 3 for Sci/engr (hon)': 'Calculus 3 for Science and Engineering (hon)',
@@ -47,7 +45,7 @@ it('standardizeClassName', function () {
     'Calculus 2 for Sci/engr (hon)': 'Calculus 2 for Science and Engineering (hon)',
     'Calculus 1 for Sci/engr (hon)': 'Calculus 1 for Science and Engineering (hon)',
     'Calculus 1for Sci/engr (hon)': 'Calculus 1 for Science and Engineering (hon)',
-    "Calc for Business/econ (hon)": 'Calculus for Business and Economics (hon)',
+    'Calc for Business/econ (hon)': 'Calculus for Business and Economics (hon)',
     'Calc & Diff Eq - Biol 1(hon)': 'Calculus and Differential Equations for Biology 1 (hon)',
 
     // econ
@@ -56,11 +54,13 @@ it('standardizeClassName', function () {
     // cs
     'Fundamental of Com Sci1': 'Fundamentals of Computer Science 1',
     'Fundamentals of Com Sci1 (hon)': 'Fundamentals of Computer Science 1 (hon)',
-    'Crisis Resolution in Mdl East': 'Crisis Resolution in Middle East'
-  }
+    'Crisis Resolution in Mdl East': 'Crisis Resolution in Middle East',
+  };
 
-  for (var badName in classNameTranslation) {
-    var goodName = classNameTranslation[badName]
+  let badName;
+
+  for (badName of Object.keys(classNameTranslation)) {
+    goodName = classNameTranslation[badName];
     expect(baseParser.standardizeClassName(badName, ['hihfdsjal', goodName])).toBe(goodName);
   }
 
@@ -70,30 +70,30 @@ it('standardizeClassName', function () {
     'Foundations of Psych': 'Foundations of Psychology',
     'Arch,infrastructure&city ': 'Architecture, Infrastructure, and the City',
     'Principles of Macroecon    (hon)   ': 'Principles of Macroeconomics (hon)',
-  }
+  };
 
 
-  for (var badName in classNameTranslation) {
-    var goodName = classNameTranslation[badName]
+  for (badName of Object.keys(classNameTranslation)) {
+    goodName = classNameTranslation[badName];
     expect(baseParser.standardizeClassName(badName, ['hihfdsjal', goodName])).toBe(goodName);
   }
 
 
-  var badName = 'Dif Eq & Lin Alg Fr Engr'
-  var possibleMatch = 'Differential Equations and Linear Algebra for Engineering (hon)'
-  var goodName = 'Differential Equations and Linear Algebra for Engineering'
+  badName = 'Dif Eq & Lin Alg Fr Engr';
+  let possibleMatch = 'Differential Equations and Linear Algebra for Engineering (hon)';
+  goodName = 'Differential Equations and Linear Algebra for Engineering';
   expect(baseParser.standardizeClassName(badName, ['hihfdsjal', possibleMatch])).toBe(goodName);
 
 
-  var badName = 'General Phys I- Lab'
-  var possibleMatch = 'General Physics I'
-  var goodName = 'General Physics I - Lab'
+  badName = 'General Phys I- Lab';
+  possibleMatch = 'General Physics I';
+  goodName = 'General Physics I - Lab';
   expect(baseParser.standardizeClassName(badName, ['hihfdsjal', possibleMatch])).toBe(goodName);
 
 
-  var badName = 'Co-op Work Experience--cj'
-  var possibleMatch = 'Co-op Work Experience-as'
-  var goodName = 'Co-op Work Experience - cj'
+  badName = 'Co-op Work Experience--cj';
+  possibleMatch = 'Co-op Work Experience-as';
+  goodName = 'Co-op Work Experience - cj';
   expect(baseParser.standardizeClassName(badName, ['hihfdsjal', possibleMatch])).toBe(goodName);
 
   expect(baseParser.standardizeClassName('hi    (yo)')).toBe('hi (yo)');
@@ -101,7 +101,7 @@ it('standardizeClassName', function () {
   expect(baseParser.standardizeClassName('hi (HON)')).toBe('hi (hon)');
 
 
-  var name = 'St: Wireless Sensor Networks'
+  const name = 'St: Wireless Sensor Networks';
   expect(baseParser.standardizeClassName(name, ['St: Intro. to Multiferroics'])).toBe(name);
 
   expect(baseParser.standardizeClassName('Directed Reading', ['Dir Rdg:'])).toBe('Directed Reading');
@@ -111,91 +111,83 @@ it('standardizeClassName', function () {
 });
 
 
-it('parseTable', async function (done) {
-
-
+it('parseTable', async (done) => {
   const body = await fs.readFile(path.join(__dirname, 'data', 'baseParser', '1.html'), 'utf8');
 
-  request.handleRequestResponce(body, function (err, dom) {
+  request.handleRequestResponce(body, (err, dom) => {
     expect(err).toBe(null);
 
     // I switched it to .rowCount and .tableData so this dosen't work yet
-    const tableDataAndRowCount = baseParser.parseTable(dom[0])
+    const tableDataAndRowCount = baseParser.parseTable(dom[0]);
 
     expect(tableDataAndRowCount).toMatchSnapshot();
 
-    done()
+    done();
   });
 });
 
 
-it('parseTable should work 2', async function (done) {
-
-
+it('parseTable should work 2', async (done) => {
   const body = await fs.readFile(path.join(__dirname, 'data', 'baseParser', '3.html'), 'utf8');
 
-  var fileJSON = JSON.parse(body);
+  const fileJSON = JSON.parse(body);
 
-  request.handleRequestResponce(fileJSON.body, function (err, dom) {
+  request.handleRequestResponce(fileJSON.body, (err, dom) => {
     expect(err).toBe(null);
 
     // I switched it to .rowCount and .tableData so this dosen't work yet
     expect(baseParser.parseTable(dom[0])).toMatchSnapshot();
-    
-    done()
+
+    done();
   });
-
 });
 
 
+const text = '\n' +
+'    1.000 TO     2.000 Credit hours\n' +
+'\n' +
+'\n' +
+'Levels: Undergraduate Transcript\n' +
+'\n' +
+'Schedule Types: Research Project\n' +
+'\n' +
+'\n' +
+'Non-Divisional Division\n' +
+'\n' +
+'Cognitive Science Department\n' +
+'\n' +
+'\n' +
+'\n' +
+'\n';
 
-var text = "\n" +
-"    1.000 TO     2.000 Credit hours\n" +
-"\n" +
-"\n" +
-"Levels: Undergraduate Transcript\n" +
-"\n" +
-"Schedule Types: Research Project\n" +
-"\n" +
-"\n" +
-"Non-Divisional Division\n" +
-"\n" +
-"Cognitive Science Department\n" +
-"\n" +
-"\n" +
-"\n" +
-"\n" 
+it('credit test', () => {
+  let creditsParsed = baseParser.parseCredits(text);
+  expect(creditsParsed.minCredits).toBe(1);
+  expect(creditsParsed.maxCredits).toBe(2);
 
-it('credit test', function() {
-  var a = baseParser.parseCredits(text)
-  expect(a.minCredits).toBe(1)
-  expect(a.maxCredits).toBe(2)
-
-  var a = baseParser.parseCredits('3 to 5 credits')
-  expect(a.minCredits).toBe(3)
-  expect(a.maxCredits).toBe(5)
-
-  // min cant be greater than max
-  var a = baseParser.parseCredits('8 to 5 credit hours')
-  expect(a.minCredits).toBe(5)
-  expect(a.maxCredits).toBe(5)
-
+  creditsParsed = baseParser.parseCredits('3 to 5 credits');
+  expect(creditsParsed.minCredits).toBe(3);
+  expect(creditsParsed.maxCredits).toBe(5);
 
   // min cant be greater than max
-  var a = baseParser.parseCredits('Credit Hours: 8.000')
-  expect(a.minCredits).toBe(8)
-  expect(a.maxCredits).toBe(8)
+  creditsParsed = baseParser.parseCredits('8 to 5 credit hours');
+  expect(creditsParsed.minCredits).toBe(5);
+  expect(creditsParsed.maxCredits).toBe(5);
 
 
+  // min cant be greater than max
+  creditsParsed = baseParser.parseCredits('Credit Hours: 8.000');
+  expect(creditsParsed.minCredits).toBe(8);
+  expect(creditsParsed.maxCredits).toBe(8);
 });
 
-it('simplifySymbols', function() {
-  var a = baseParser.simplifySymbols('‚‚‚„„„')
-  expect(a).toBe('\'\'\'"""')
-});   
+it('simplifySymbols', () => {
+  const a = baseParser.simplifySymbols('‚‚‚„„„');
+  expect(a).toBe('\'\'\'"""');
+});
 
-it('credit parser can parse Continuing Education Units ', function() {
-  var credits = baseParser.parseCredits('// 0.800 Continuing Education Units ')
-  expect(credits.minCredits).toBe(.8)
-  expect(credits.maxCredits).toBe(.8)
+it('credit parser can parse Continuing Education Units ', () => {
+  const credits = baseParser.parseCredits('// 0.800 Continuing Education Units ');
+  expect(credits.minCredits).toBe(0.8);
+  expect(credits.maxCredits).toBe(0.8);
 });
