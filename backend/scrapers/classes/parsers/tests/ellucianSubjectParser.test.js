885ce8838b366331eeb8a9206780d8c5d6094caf
Ryan Hughes <ryanhughes624@gmail.com>
2017-07-24 21:54:35 -0700

    subject parser tests

--- backend/scrapers/classes/parsers/tests/ellucianSubjectParser.test.js
+++ backend/scrapers/classes/parsers/tests/ellucianSubjectParser.test.js
@@ -13,89 +13,70 @@
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>. 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 
 import path from 'path';
-import request from '../../../request';
+import fs from 'fs-promise';
 
-var ellucianSubjectParser = require('../ellucianSubjectParser')
-var fs = require('fs')
-var PageData = require('../../PageData')
-var URI = require('urijs')
+import ellucianSubjectParser from '../ellucianSubjectParser';
 
 
-it('should work', function (done) {
+it('should work', async (done) => {
+  const body = await fs.readFile(path.join(__dirname, 'data', 'ellucianSubjectParser', '1.html'), 'utf8');
+
+  const url = 'https://bannerweb.upstate.edu/isis/bwckgens.p_proc_term_date';
+
+  expect(true).toBe(ellucianSubjectParser.supportsPage(url));
+
+  const output = ellucianSubjectParser.parse(body, url);
+
+  expect(output.length).toBe(27);
+
+  expect(output[0].value).toEqual({
+    subject: 'ANAT',
+    text: 'Anatomy CM',
+  });
+
+  // expect(3)
+  // expect().toBe(ellucianSubjectParser)
 
 
-	fs.readFile(path.join(__dirname, 'data', 'ellucianSubjectParser', '1.html'), 'utf8', function (err, body) {
-		expect(err).toBe(null);
+  // console.log(pageData.deps)
+  // assert.deepEqual(pageData.dbData,{ url: 'https://bannerweb.upstate.edu/isis/bwckgens.p_proc_term_date',
+  //  subjects:
+  //  [ { id: 'ANAT', text: 'Anatomy CM' },
+  //  { id: 'ANES', text: 'Anesthesiology CM' },
+  //  { id: 'CBHX', text: 'Bioethics and Humanities' },
+  //  { id: 'CCFM', text: 'Consortium - Culture/Medicine' },
+  //  { id: 'EMED', text: 'Emergency Medicine CM&HP' },
+  //  { id: 'FAMP', text: 'Family Medicine CM' },
+  //  { id: 'GERI', text: 'Geriatrics CM' },
+  //  { id: 'INTD', text: 'Interdepartmental CM&HP' },
+  //  { id: 'INTL', text: 'International Experience' },
+  //  { id: 'MDCN', text: 'Medicine CM' },
+  //  { id: 'MICB', text: 'Microbiology CM' },
+  //  { id: 'M', text: 'Microbiology and Immunology GS' }, //ellucianSubjectParser is same as html
+  //  { id: 'NEUR', text: 'Neurology CM' },
+  //  { id: 'NSUG', text: 'Neurosurgery CM' },
+  //  { id: 'OBGY', text: 'Obstetrics and Gynecology CM' },
+  //  { id: 'OPTH', text: 'Opthalmology CM' },
+  //  { id: 'ORTH', text: 'Orthopaedic Surgery CM' },
+  //  { id: 'OTOL', text: 'Otolaryngology CM' },
+  //  { id: 'PATH', text: 'Pathology CM&HP' },
+  //  { id: 'PEDS', text: 'Pediatrics CM' },
+  //  { id: 'RMED', text: 'Physical Med/Rehabilitation CM' },
+  //  { id: 'PRVM', text: 'Preventive Medicine' },
+  //  { id: 'PYCH', text: 'Psychiatry CM' },
+  //  { id: 'RONC', text: 'Radiation Oncology CM' },
+  //  { id: 'RADL', text: 'Radiology CM' },
+  //  { id: 'SURG', text: 'Surgery CM' },
+  //  { id: 'UROL', text: 'Urology CM' } ],
+  //  termId: '201510',
+  //  host: 'upstate.edu' });
 
-		request.handleRequestResponce(body, function (err, dom) {
-			expect(err).toBe(null);
+  // //
 
-			var url = 'https://bannerweb.upstate.edu/isis/bwckgens.p_proc_term_date';
-
-			expect(true).toBe(ellucianSubjectParser.supportsPage(url));
-
-			var pageData = PageData.create({
-				dbData: {
-					url: url
-				}
-			});
-
-			ellucianSubjectParser.parseDOM(pageData, dom);
-
-			expect(pageData.deps.length).toBe(27)
-
-			expect(pageData.deps[0].dbData).toEqual({
-				updatedByParent: true,
-				subject: 'ANAT',
-				text: 'Anatomy CM'
-			})
-
-			// expect(3)
-			// expect().toBe(ellucianSubjectParser)
-
-
-			// console.log(pageData.deps)
-			// assert.deepEqual(pageData.dbData,{ url: 'https://bannerweb.upstate.edu/isis/bwckgens.p_proc_term_date',
-			// 	subjects:
-			// 	[ { id: 'ANAT', text: 'Anatomy CM' },
-			// 	{ id: 'ANES', text: 'Anesthesiology CM' },
-			// 	{ id: 'CBHX', text: 'Bioethics and Humanities' },
-			// 	{ id: 'CCFM', text: 'Consortium - Culture/Medicine' },
-			// 	{ id: 'EMED', text: 'Emergency Medicine CM&HP' },
-			// 	{ id: 'FAMP', text: 'Family Medicine CM' },
-			// 	{ id: 'GERI', text: 'Geriatrics CM' },
-			// 	{ id: 'INTD', text: 'Interdepartmental CM&HP' },
-			// 	{ id: 'INTL', text: 'International Experience' },
-			// 	{ id: 'MDCN', text: 'Medicine CM' },
-			// 	{ id: 'MICB', text: 'Microbiology CM' },
-			// 	{ id: 'M', text: 'Microbiology and Immunology GS' }, //ellucianSubjectParser is same as html
-			// 	{ id: 'NEUR', text: 'Neurology CM' },
-			// 	{ id: 'NSUG', text: 'Neurosurgery CM' },
-			// 	{ id: 'OBGY', text: 'Obstetrics and Gynecology CM' },
-			// 	{ id: 'OPTH', text: 'Opthalmology CM' },
-			// 	{ id: 'ORTH', text: 'Orthopaedic Surgery CM' },
-			// 	{ id: 'OTOL', text: 'Otolaryngology CM' },
-			// 	{ id: 'PATH', text: 'Pathology CM&HP' },
-			// 	{ id: 'PEDS', text: 'Pediatrics CM' },
-			// 	{ id: 'RMED', text: 'Physical Med/Rehabilitation CM' },
-			// 	{ id: 'PRVM', text: 'Preventive Medicine' },
-			// 	{ id: 'PYCH', text: 'Psychiatry CM' },
-			// 	{ id: 'RONC', text: 'Radiation Oncology CM' },
-			// 	{ id: 'RADL', text: 'Radiology CM' },
-			// 	{ id: 'SURG', text: 'Surgery CM' },
-			// 	{ id: 'UROL', text: 'Urology CM' } ],
-			// 	termId: '201510',
-			// 	host: 'upstate.edu' });
-
-			// //
-
-			done()
-		});
-	});
-
+  done();
 });
