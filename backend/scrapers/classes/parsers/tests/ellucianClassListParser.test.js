3a04e98c161598d670425eb1b4a7dfa3b52f741b
Ryan Hughes <ryanhughes624@gmail.com>
2017-07-24 21:58:46 -0700

    classs list tests

--- backend/scrapers/classes/parsers/tests/ellucianClassListParser.test.js
+++ backend/scrapers/classes/parsers/tests/ellucianClassListParser.test.js
@@ -13,45 +13,27 @@
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>. 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import request from '../../../request'
-import path from 'path'
+import path from 'path';
+import fs from 'fs-promise';
 
-var ellucianCatalogParser = require('../ellucianCatalogParser')
-var ellucianClassListParser = require('../ellucianClassListParser')
-var PageData = require('../../PageData')
-var fs = require('fs')
-var URI = require('urijs')
+import ellucianClassListParser from '../ellucianClassListParser';
 
-it('should behave...', function(done) {
-	
-	fs.readFile(path.join(__dirname, 'data', 'ellucianClassListParser', '2.html'), 'utf8', function (err, body) {
-		expect(err).toBe(null);
+it('should behave...', async (done) => {
+  const body = await fs.readFile(path.join(__dirname, 'data', 'ellucianClassListParser', '2.html'), 'utf8');
 
-		request.handleRequestResponce(body, function (err, dom) {
-			expect(err).toBe(null);
+  const catalogURL = 'https://bannerweb.upstate.edu/isis/bwckctlg.p_disp_course_detail?cat_term_in=201580&subj_code_in=MDCN&crse_numb_in=2064';
 
-			var url = 'https://bannerweb.upstate.edu/isis/bwckctlg.p_display_courses?term_in=201580&one_subj=MDCN&sel_crse_strt=2064&sel_crse_end=2064&sel_subj=&sel_levl=&sel_schd=&sel_coll=&sel_divs=&sel_dept=&sel_attr=';
 
-			var catalogURL = "https://bannerweb.upstate.edu/isis/bwckctlg.p_disp_course_detail?cat_term_in=201580&subj_code_in=MDCN&crse_numb_in=2064";
+  const url = 'https://bannerweb.upstate.edu/isis/bwckctlg.p_display_courses?term_in=201580&one_subj=MDCN&sel_crse_strt=2064&sel_crse_end=2064&sel_subj=&sel_levl=&sel_schd=&sel_coll=&sel_divs=&sel_dept=&sel_attr=';
 
-			expect(true).toBe(ellucianClassListParser.supportsPage(url));
+  expect(true).toBe(ellucianClassListParser.supportsPage(url));
 
-			var pageData = PageData.create({
-				dbData: {
-					url: url,
-					subject: 'MATH',
-					termId: '201504'
-				}
-			});
+  const output = ellucianClassListParser.parse(body, url);
 
-			ellucianClassListParser.parseDOM(pageData, dom);
-
-			expect(pageData.deps.length).toBe(1);
-			expect(pageData.deps[0].dbData.url).toBe(catalogURL)
-			done()
-		});
-	});
-}); 
+  expect(output.length).toBe(1);
+  expect(output[0]).toBe(catalogURL);
+  done();
+});
