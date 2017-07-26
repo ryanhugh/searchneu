8da187391978450f1a17a5db76391b9970a9e243
Ryan Hughes <ryanhughes624@gmail.com>
2017-07-25 18:15:12 -0700

    .

--- backend/scrapers/classes/parsers/tests/ellucianTermsParser.test.js
+++ backend/scrapers/classes/parsers/tests/ellucianTermsParser.test.js
@@ -13,70 +13,61 @@
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>. 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 import path from 'path';
 import MockDate from 'mockdate';
 import fs from 'fs-promise';
-import URI from 'urijs';
-import cheerio from 'cheerio';
 
-import ellucianCatalogParser from '../ellucianCatalogParser';
 import ellucianTermsParser from '../ellucianTermsParser';
 
 
-beforeAll(function () {
+beforeAll(() => {
   MockDate.set('10/10/2016');
 });
 
-afterAll(function() {
+afterAll(() => {
   MockDate.reset();
 });
 
 
-
-it('has a name', function() {
-  
+it('has a name', () => {
   //make sure a name is defined
   expect(ellucianTermsParser.name);
 });
 
-it('isValidTerm should work', function () {
-
-  expect(ellucianTermsParser.isValidTerm('201630', 'blah blah 2016')).toBe(true)
-  expect(ellucianTermsParser.isValidTerm('201630', 'blah blah 2017')).toBe(true)
-  expect(ellucianTermsParser.isValidTerm('201630', 'blah blah')).toBe(true)
-  expect(ellucianTermsParser.isValidTerm('2016', 'blah blah')).toBe(true)
-  expect(ellucianTermsParser.isValidTerm('201', 'blah blah')).toBe(false)
+it('isValidTerm should work', () => {
+  expect(ellucianTermsParser.isValidTerm('201630', 'blah blah 2016')).toBe(true);
+  expect(ellucianTermsParser.isValidTerm('201630', 'blah blah 2017')).toBe(true);
+  expect(ellucianTermsParser.isValidTerm('201630', 'blah blah')).toBe(true);
+  expect(ellucianTermsParser.isValidTerm('2016', 'blah blah')).toBe(true);
+  expect(ellucianTermsParser.isValidTerm('201', 'blah blah')).toBe(false);
 });
-  
-it('should behave...', async function (done) {
 
+it('should behave...', async (done) => {
   const body = await fs.readFile(path.join(__dirname, 'data', 'ellucianTermsParser', '1.html'), 'utf8');
 
-  var url = 'https://bannerweb.upstate.edu/isis/bwckschd.p_disp_dyn_sched';
+  const url = 'https://bannerweb.upstate.edu/isis/bwckschd.p_disp_dyn_sched';
   expect(true).toBe(ellucianTermsParser.supportsPage(url));
 
   expect(ellucianTermsParser.parse(body, url)).toMatchSnapshot();
 
-  done()
+  done();
 });
 
 
-
-it('hi there 2', async function (done) {
-
+it('hi there 2', async (done) => {
   const body = await fs.readFile(path.join(__dirname, 'data', 'ellucianTermsParser', '2.html'), 'utf8');
 
-  var url = 'https://wl11gp.neu.edu/udcprod8/bwckschd.p_disp_dyn_sched';
+  const url = 'https://wl11gp.neu.edu/udcprod8/bwckschd.p_disp_dyn_sched';
 
-  let output = ellucianTermsParser.parse(body, url);
+  const output = ellucianTermsParser.parse(body, url);
 
   expect(true).toBe(ellucianTermsParser.supportsPage(url));
 
   expect(output.terms.length).toBe(17);
 
-  done()
+  done();
 });
 
