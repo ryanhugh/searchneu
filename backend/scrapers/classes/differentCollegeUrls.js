76b9e8dc505a402a7ac067c1a2601b93a4206763
Ryan Hughes <ryanhughes624@gmail.com>
2017-07-25 20:01:57 -0700

    .

--- backend/scrapers/classes/differentCollegeUrls.js
+++ backend/scrapers/classes/differentCollegeUrls.js
@@ -13,7 +13,7 @@
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>. 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 // How to find more sites.
@@ -28,75 +28,75 @@
 // and check to see if it the server running the registration system
 // can check for link to https://<ip here>/css/web_defaultapp.css
 // which is the same for all the links
-// Two ways to rdns: actual rdns, and then look at the https cert for the url it presented. 
-// Was able to go from dccc.edu to their site with this method. Will take longer for bigger schools. 
+// Two ways to rdns: actual rdns, and then look at the https cert for the url it presented.
+// Was able to go from dccc.edu to their site with this method. Will take longer for bigger schools.
 
 
-// At one point there was a couple problems with hitting https sites on port other than 443, but I think the problem was something that 
-// all the sites had and not related to what the port number was. idk. Could make http requests to those sites from python and the browser and curl, but not node. 
+// At one point there was a couple problems with hitting https sites on port other than 443, but I think the problem was something that
+// all the sites had and not related to what the port number was. idk. Could make http requests to those sites from python and the browser and curl, but not node.
 
 // Some other ways to find subdomains:
 // Wolfram alpha, but idk if they have an free API and it is not a full list, idk where they are getting the data from
 // https://github.com/faizann24/XssPy
-//     Havent looked into this much. 
-//     They mentioned that they have some way of getting subdomains though. Looks like it just spiders the site, which can totally work. 
-//     Some sites have robots.txt set to deny so Google and Bing do not spider it. 
+//     Havent looked into this much.
+//     They mentioned that they have some way of getting subdomains though. Looks like it just spiders the site, which can totally work.
+//     Some sites have robots.txt set to deny so Google and Bing do not spider it.
 
- module.exports = ["https://oscar.gatech.edu/pls/bprod/bwckschd.p_disp_dyn_sched", //works
-  "https://wl11gp.neu.edu/udcprod8/bwckschd.p_disp_dyn_sched", //works
-  "https://sisssb.clemson.edu/sisbnprd/bwckschd.p_disp_dyn_sched", //works
-  "https://ssb.ccsu.edu/pls/ssb_cPROD/bwckschd.p_disp_dyn_sched", //works
-  "https://ssb.cc.binghamton.edu/banner/bwckschd.p_disp_dyn_sched", //this one dosen't have this url (scroll right), figure out which parser needs this and if it would be a easy fix or naw https://ssb.cc.binghamton.edu/banner/bwckctlg.p_disp_listcrse?term_in=201510&subj_in=MDVL&crse_in=561B&schd_in=%25
-  "https://tturedss1.tntech.edu/pls/PROD/bwckschd.p_disp_dyn_sched", //ran with bingham, and only got 1 term and 0 subjects... idk. will finish quick if ran again probably. Ran again in Aug 2016 and it looks like the term farthest in the future is added, but dosen't have any subjects. Other terms are valid. 
-  "https://prd-wlssb.temple.edu/prod8/bwckschd.p_disp_dyn_sched", //takes 3 hours and prereqs are not parsed correctly, but everything else works
-  "https://bannerweb.upstate.edu/isis/bwckschd.p_disp_dyn_sched", //works
-  "https://ssb.banner.usu.edu/zprod/bwckschd.p_disp_dyn_sched", //works
-  "https://ssbprod11g.uncfsu.edu/pls/FSUPROD/bwckschd.p_disp_dyn_sched", // down atm?? down march 10, 2016. ports 80 and 443 closed, others filtered. looks like firewall still up but app down.
-  "https://banner.uregina.ca/prod/sct/bwckschd.p_disp_dyn_sched", //takes 3 hours, works
-  "https://banners.presby.edu/prod/bwckschd.p_disp_dyn_sched", // takes like 22 min, works
-  "https://eagles.tamut.edu/texp/bwckschd.p_disp_dyn_sched", // collegeName capilization bug (fixed) and one subject dosent have any results, but works
-  "https://ssb.sju.edu/pls/PRODSSB/bwckschd.p_disp_dyn_sched", //works
-  "https://sail.oakland.edu/PROD/bwckschd.p_disp_dyn_sched", //works, takes 3 hours
-  "https://genisys.regent.edu/pls/prod/bwckschd.p_disp_dyn_sched",
-  "https://bappas2.gram.edu:9000/pls/gram/bwckschd.p_disp_dyn_sched", //dosent work in nodejs!!!!??? works in python, browser, etc
-  "https://sail.oakland.edu/PROD/bwckschd.p_disp_dyn_sched", // works 8/31/16, some minor errors in trello spider but nothing major 
-  "https://banweb.wm.edu/pls/PROD/bwckschd.p_disp_dyn_sched", // thier site worked on 8/31/16, but need to update spider code before can use (fatal errors)
-  "https://prod-ssb-01.dccc.edu/PROD/bwckschd.p_disp_dyn_sched", //worked in like dec 2015 and limited rate to about 100 connections at once.
-  "https://selfservice.mypurdue.purdue.edu/prod/bwckschd.p_disp_dyn_sched", // i think i fixed the bug. need to test again. 8/31/16
-  "https://xbss-prod.lasalle.edu/PROD/bwckschd.p_disp_dyn_sched", // works 8/31/16
-  "https://myswat.swarthmore.edu/pls/bwckschd.p_disp_dyn_sched", //works
-  "https://lewisweb.cc.lehigh.edu/PROD/bwckschd.p_disp_dyn_sched", //works 8/31/16, there are 3 classes that the prereq parser cant get because they have messed up parens. 
-  "https://myweb.du.edu/mdb/bwckschd.p_disp_dyn_sched", // 8/31/16 uhhhhhhhh not really sure, it was working and then i mucked with mongodb indexes and it stopped. have mucked with indexes during spidering before so it might be something on the site that caused the code to stop. need to retry. 
-  "https://ssbprod.rcgc.edu:9000/prod_ssb/bwckschd.p_disp_dyn_sched", // school aparently changed their hostname, it was gcc-ssbprod.gccnj.edu and now for the old domain dns fails now, wayback machine has it working in 2015: https://web.archive.org/web/20150316171359/https://gcc-ssbprod.gccnj.edu:9000/prod_ssb/bwckschd.p_disp_dyn_sched
-  "https://pssb.stockton.edu/prod/bwckschd.p_disp_dyn_sched", //was port 9000, now is 443
-  "https://www8.unm.edu/pls/banp/bwckschd.p_disp_dyn_sched", // took about 3 hours but it worked without any errors. 8/31/16
-  "https://banssbprod.xavier.edu:8099/PROD/bwckschd.p_disp_dyn_sched", // works 9/1/16 took about 40 min
-  "https://appprod.udayton.edu:9000/prod/bwckschd.p_disp_dyn_sched", // worked 9/1/16, 4 prereq parsing issues but besides that worked perfectly.
-  "https://ui2web4.apps.uillinois.edu/BANPROD4/bwckschd.p_disp_dyn_sched", // worked 9/2/16 no errors
-  "https://lpar2.jsu.edu/DADNormalPRO8/bwckschd.p_disp_dyn_sched", //  worked 9/2/16 no errors
-  "https://ssb.isu.edu/bprod/bwckschd.p_disp_dyn_sched", //  worked 9/2/16 no errors
-  "https://ssb.columbiastate.edu/PROD/bwckschd.p_disp_dyn_sched", // worked dec 27th, 2016
-  "https://selfservice.brown.edu/ss/bwckschd.p_disp_dyn_sched", // uses 12GB RAM, works. Dec 28th.  Dec mongo inserting errors on 9/2/16, need to try again.
-  "https://ssbprod.wichita.edu/PROD/bwckschd.p_disp_dyn_sched",
-  "https://prodssb.mscc.edu/PROD/bwckschd.p_disp_dyn_sched",
-  "https://ssbprod11g.uncfsu.edu/pls/FSUPROD/bwckschd.p_disp_dyn_sched",
-  "https://banner4.utm.edu/prod/bwckschd.p_disp_dyn_sched",
-  "https://nssb-p.adm.fit.edu/prod/bwckschd.p_disp_dyn_sched",
-  "https://prodssb.ws.edu/pls/PROD/bwckschd.p_disp_dyn_sched",
-  "https://oasis.farmingdale.edu/banner/bwckschd.p_disp_dyn_sched",
-  "https://oxford.blinn.edu:9010/PROD/bwckschd.p_disp_dyn_sched",
-  "https://ssb.ship.edu/prod/bwckschd.p_disp_dyn_sched",
-  "https://telaris.wlu.ca/ssb_prod/bwckschd.p_disp_dyn_sched",
-  "https://www2.augustatech.edu/pls/ban8/bwckschd.p_disp_dyn_sched",
-  "https://banner.drexel.edu/pls/duprod/bwckschd.p_disp_dyn_sched",
-  "https://infobear.bridgew.edu/BANP/bwckschd.p_disp_dyn_sched",
-  "https://new-sis-app.sph.harvard.edu:9010/prod/bwckschd.p_disp_dyn_sched", // This is Harvard's School of public health, not the Undergraduate College. 
-  "https://novasis.villanova.edu/pls/bannerprd/bwckschd.p_disp_dyn_sched"
-]
+ module.exports = ['https://oscar.gatech.edu/pls/bprod/bwckschd.p_disp_dyn_sched', //works
+   'https://wl11gp.neu.edu/udcprod8/bwckschd.p_disp_dyn_sched', //works
+   'https://sisssb.clemson.edu/sisbnprd/bwckschd.p_disp_dyn_sched', //works
+   'https://ssb.ccsu.edu/pls/ssb_cPROD/bwckschd.p_disp_dyn_sched', //works
+   'https://ssb.cc.binghamton.edu/banner/bwckschd.p_disp_dyn_sched', //this one dosen't have this url (scroll right), figure out which parser needs this and if it would be a easy fix or naw https://ssb.cc.binghamton.edu/banner/bwckctlg.p_disp_listcrse?term_in=201510&subj_in=MDVL&crse_in=561B&schd_in=%25
+   'https://tturedss1.tntech.edu/pls/PROD/bwckschd.p_disp_dyn_sched', //ran with bingham, and only got 1 term and 0 subjects... idk. will finish quick if ran again probably. Ran again in Aug 2016 and it looks like the term farthest in the future is added, but dosen't have any subjects. Other terms are valid.
+   'https://prd-wlssb.temple.edu/prod8/bwckschd.p_disp_dyn_sched', //takes 3 hours and prereqs are not parsed correctly, but everything else works
+   'https://bannerweb.upstate.edu/isis/bwckschd.p_disp_dyn_sched', //works
+   'https://ssb.banner.usu.edu/zprod/bwckschd.p_disp_dyn_sched', //works
+   'https://ssbprod11g.uncfsu.edu/pls/FSUPROD/bwckschd.p_disp_dyn_sched', // down atm?? down march 10, 2016. ports 80 and 443 closed, others filtered. looks like firewall still up but app down.
+   'https://banner.uregina.ca/prod/sct/bwckschd.p_disp_dyn_sched', //takes 3 hours, works
+   'https://banners.presby.edu/prod/bwckschd.p_disp_dyn_sched', // takes like 22 min, works
+   'https://eagles.tamut.edu/texp/bwckschd.p_disp_dyn_sched', // collegeName capilization bug (fixed) and one subject dosent have any results, but works
+   'https://ssb.sju.edu/pls/PRODSSB/bwckschd.p_disp_dyn_sched', //works
+   'https://sail.oakland.edu/PROD/bwckschd.p_disp_dyn_sched', //works, takes 3 hours
+   'https://genisys.regent.edu/pls/prod/bwckschd.p_disp_dyn_sched',
+   'https://bappas2.gram.edu:9000/pls/gram/bwckschd.p_disp_dyn_sched', //dosent work in nodejs!!!!??? works in python, browser, etc
+   'https://sail.oakland.edu/PROD/bwckschd.p_disp_dyn_sched', // works 8/31/16, some minor errors in trello spider but nothing major
+   'https://banweb.wm.edu/pls/PROD/bwckschd.p_disp_dyn_sched', // thier site worked on 8/31/16, but need to update spider code before can use (fatal errors)
+   'https://prod-ssb-01.dccc.edu/PROD/bwckschd.p_disp_dyn_sched', //worked in like dec 2015 and limited rate to about 100 connections at once.
+   'https://selfservice.mypurdue.purdue.edu/prod/bwckschd.p_disp_dyn_sched', // i think i fixed the bug. need to test again. 8/31/16
+   'https://xbss-prod.lasalle.edu/PROD/bwckschd.p_disp_dyn_sched', // works 8/31/16
+   'https://myswat.swarthmore.edu/pls/bwckschd.p_disp_dyn_sched', //works
+   'https://lewisweb.cc.lehigh.edu/PROD/bwckschd.p_disp_dyn_sched', //works 8/31/16, there are 3 classes that the prereq parser cant get because they have messed up parens.
+   'https://myweb.du.edu/mdb/bwckschd.p_disp_dyn_sched', // 8/31/16 uhhhhhhhh not really sure, it was working and then i mucked with mongodb indexes and it stopped. have mucked with indexes during spidering before so it might be something on the site that caused the code to stop. need to retry.
+   'https://ssbprod.rcgc.edu:9000/prod_ssb/bwckschd.p_disp_dyn_sched', // school aparently changed their hostname, it was gcc-ssbprod.gccnj.edu and now for the old domain dns fails now, wayback machine has it working in 2015: https://web.archive.org/web/20150316171359/https://gcc-ssbprod.gccnj.edu:9000/prod_ssb/bwckschd.p_disp_dyn_sched
+   'https://pssb.stockton.edu/prod/bwckschd.p_disp_dyn_sched', //was port 9000, now is 443
+   'https://www8.unm.edu/pls/banp/bwckschd.p_disp_dyn_sched', // took about 3 hours but it worked without any errors. 8/31/16
+   'https://banssbprod.xavier.edu:8099/PROD/bwckschd.p_disp_dyn_sched', // works 9/1/16 took about 40 min
+   'https://appprod.udayton.edu:9000/prod/bwckschd.p_disp_dyn_sched', // worked 9/1/16, 4 prereq parsing issues but besides that worked perfectly.
+   'https://ui2web4.apps.uillinois.edu/BANPROD4/bwckschd.p_disp_dyn_sched', // worked 9/2/16 no errors
+   'https://lpar2.jsu.edu/DADNormalPRO8/bwckschd.p_disp_dyn_sched', //  worked 9/2/16 no errors
+   'https://ssb.isu.edu/bprod/bwckschd.p_disp_dyn_sched', //  worked 9/2/16 no errors
+   'https://ssb.columbiastate.edu/PROD/bwckschd.p_disp_dyn_sched', // worked dec 27th, 2016
+   'https://selfservice.brown.edu/ss/bwckschd.p_disp_dyn_sched', // uses 12GB RAM, works. Dec 28th.  Dec mongo inserting errors on 9/2/16, need to try again.
+   'https://ssbprod.wichita.edu/PROD/bwckschd.p_disp_dyn_sched',
+   'https://prodssb.mscc.edu/PROD/bwckschd.p_disp_dyn_sched',
+   'https://ssbprod11g.uncfsu.edu/pls/FSUPROD/bwckschd.p_disp_dyn_sched',
+   'https://banner4.utm.edu/prod/bwckschd.p_disp_dyn_sched',
+   'https://nssb-p.adm.fit.edu/prod/bwckschd.p_disp_dyn_sched',
+   'https://prodssb.ws.edu/pls/PROD/bwckschd.p_disp_dyn_sched',
+   'https://oasis.farmingdale.edu/banner/bwckschd.p_disp_dyn_sched',
+   'https://oxford.blinn.edu:9010/PROD/bwckschd.p_disp_dyn_sched',
+   'https://ssb.ship.edu/prod/bwckschd.p_disp_dyn_sched',
+   'https://telaris.wlu.ca/ssb_prod/bwckschd.p_disp_dyn_sched',
+   'https://www2.augustatech.edu/pls/ban8/bwckschd.p_disp_dyn_sched',
+   'https://banner.drexel.edu/pls/duprod/bwckschd.p_disp_dyn_sched',
+   'https://infobear.bridgew.edu/BANP/bwckschd.p_disp_dyn_sched',
+   'https://new-sis-app.sph.harvard.edu:9010/prod/bwckschd.p_disp_dyn_sched', // This is Harvard's School of public health, not the Undergraduate College.
+   'https://novasis.villanova.edu/pls/bannerprd/bwckschd.p_disp_dyn_sched',
+ ];
 
 // https://ssb.vcu.edu/proddad/bwckschd.p_disp_dyn_sched
 
-// This is the list of colleges on coursicle.com as of Dec 20th, 2016. Looks like they have the scrapers for the same types as sites I'm scraping here. 
+// This is the list of colleges on coursicle.com as of Dec 20th, 2016. Looks like they have the scrapers for the same types as sites I'm scraping here.
 // Auburn University – Auburn
 // Auburn University at Montgomery – AUM
 // Samford University – Samford
@@ -275,15 +275,11 @@
 // West Virginia University – WVU
 
 
-
-
-
 // non -ellucian sites
 // harvard https://courses.harvard.edu/search?sort=course_title+asc&start=0&submit=Search&rows=500000&q=0
 // if you remove the q param from the url, 15000+ courses come up, but cs50 is not one of those. If you search for cs50, it comes up. how to get all classes?
 
 //mit http://student.mit.edu/catalog/extsearch.cgi
-
 
 
 // https://banners.presby.edu/prod/hzskschd.P_ViewSchedule
@@ -292,8 +288,7 @@
 
 // https://hofstraonline.hofstra.edu/pls/HPRO/bwckschd.p_disp_dyn_sched
 // uses something similar to neu, but the page after the select subject page is a lot different... idk
-// 
-
+//
 
 
 // http://catalog.kettering.edu/coursesaz/undergrad/chem/
