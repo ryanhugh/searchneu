8a79e1a088553ebfe6bbbaafdb91ac3db45ce218
Ryan Hughes <ryanhughes624@gmail.com>
2017-07-25 20:00:29 -0700

    .

--- backend/scrapers/classes/processors/simplifyProfList.js
+++ backend/scrapers/classes/processors/simplifyProfList.js
@@ -13,12 +13,9 @@
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>. 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import mkdirp from 'mkdirp';
-import fs from 'fs';
-import path from 'path';
 import _ from 'lodash';
 
 import BaseProcessor from './baseProcessor';
@@ -28,97 +25,90 @@ class SimplifyProfList extends BaseProcessor.BaseProcessor {
 
 
   go(termDump) {
+    const updatedSections = [];
 
-    var updatedSections = []
-
-    var sectionGrouped = this.groupSectionsByClass(termDump.sections);
-    sectionGrouped.forEach(function (sectionGroup) {
-
-      var profCount = {}
+    const sectionGrouped = this.groupSectionsByClass(termDump.sections);
+    sectionGrouped.forEach((sectionGroup) => {
+      const profCount = {};
 
       // Populate prof count. Counts if prof is present in any meetings in a section
-      sectionGroup.forEach(function (section) {
+      sectionGroup.forEach((section) => {
         if (!section.meetings) {
           return;
         }
 
-        var thisSectionProfs = []
+        const thisSectionProfs = [];
 
 
-        section.meetings.forEach(function (meeting) {
+        section.meetings.forEach((meeting) => {
           if (!meeting.profs) {
             return;
           }
 
           // Keep a reference to the full list of professors
           if (!meeting.allProfs) {
-            meeting.allProfs = meeting.profs.slice(0)
+            meeting.allProfs = meeting.profs.slice(0);
           }
 
-          meeting.profs.forEach(function (prof) {
+          meeting.profs.forEach((prof) => {
             if (!thisSectionProfs.includes(prof)) {
-              thisSectionProfs.push(prof)
+              thisSectionProfs.push(prof);
             }
-          }.bind(this))
-        }.bind(this))
+          });
+        });
 
 
-
-        thisSectionProfs.forEach(function (prof) {
-          var id = section.host
+        thisSectionProfs.forEach((prof) => {
           if (!profCount[prof]) {
-            profCount[prof] = 0
+            profCount[prof] = 0;
           }
           profCount[prof]++;
-        }.bind(this))
-      }.bind(this))
+        });
+      });
 
-      if (sectionGroup.length == 1) {
+      if (sectionGroup.length === 1) {
         return;
       }
 
-      var profsOnEverySection = [];
+      const profsOnEverySection = [];
 
       // Find the profs that are listed on every section
-      for (var prof in profCount) {
-        if (profCount[prof] == sectionGroup.length) {
-          profsOnEverySection.push(prof)
+      for (const prof in profCount) {
+        if (profCount[prof] === sectionGroup.length) {
+          profsOnEverySection.push(prof);
         }
       }
 
 
       // Remove any prof in prof count that is present in all sectionGroup and when there are other profs also listed on the same section
-      sectionGroup.forEach(function (section) {
-
-        var count = 0;
+      sectionGroup.forEach((section) => {
+        let count = 0;
 
         if (!section.meetings) {
           return;
         }
 
-        section.meetings.forEach(function (meeting) {
-
-          // If all the professors in this meeting are on every meeting, don't remove any of them.  
-          if (meeting.profs.length == profsOnEverySection.length) {
+        section.meetings.forEach((meeting) => {
+          // If all the professors in this meeting are on every meeting, don't remove any of them.
+          if (meeting.profs.length === profsOnEverySection.length) {
             return;
           }
 
-          profsOnEverySection.forEach(function (prof) {
+          profsOnEverySection.forEach((prof) => {
             if (meeting.profs.length > 1) {
               count++;
-              _.pull(meeting.profs, prof)
+              _.pull(meeting.profs, prof);
             }
-          }.bind(this))
-        }.bind(this))
+          });
+        });
 
         if (count > 0) {
-          updatedSections.push(section)
-          console.log("Removing ", count, 'profs from ', section.classUid, section.crn);
+          updatedSections.push(section);
+          console.log('Removing ', count, 'profs from ', section.classUid, section.crn);
         }
+      });
+    });
 
-      }.bind(this))
-    }.bind(this))
-    
     return termDump;
   }
 }
--- backend/scrapers/classes/processors/termStartEndDate.js
+++ backend/scrapers/classes/processors/termStartEndDate.js
@@ -17,114 +17,109 @@
  */
 
 
-import macros from '../../../macros'
-const BaseProcessor = require('./baseProcessor').BaseProcessor;
-const _ = require('lodash');
+import _ from 'lodash';
 
+import macros from '../../../macros';
+import BaseProcessor from './baseProcessor';
 
 // This file adds startDate and endDate to each term based on the start and end dates in sections in that term
 // The start date is the first date that over 10% of sections start on, and the end is the last date that over 10% of sections end on
 // If no one date has over 10% sections start on that date, it is just the first/last date
 
+class TermStartEndDate extends BaseProcessor.BaseProcessor {
 
-function TermStartEndDate() {
-  BaseProcessor.prototype.constructor.apply(this, arguments);
+
+  runOnTerm(termDump, term) {
+    const startDates = {};
+    const endDates = {};
+    let meetingCount = 0;
+
+    if (termDump.sections.length === 0) {
+      console.error('No sections in db???', termDump.sections);
+    }
+
+    termDump.sections.forEach((section) => {
+      if (section.termId !== term.termId) {
+        return;
+      }
+
+      if (section.meetings) {
+        section.meetings.forEach((meeting) => {
+          if (startDates[meeting.startDate] === undefined) {
+            startDates[meeting.startDate] = 0;
+          }
+          startDates[meeting.startDate]++;
+
+          if (endDates[meeting.endDate] === undefined) {
+            endDates[meeting.endDate] = 0;
+          }
+          endDates[meeting.endDate]++;
+          meetingCount++;
+        });
+      }
+    });
+
+    let finalStartDate;
+    let finalEndDate;
+
+    const startDateKeys = _.keys(startDates).sort((a, b) => {
+      return parseInt(a, 10) - parseInt(b, 10);
+    });
+
+    for (let i = 0; i < startDateKeys.length; i++) {
+      const date = startDateKeys[i];
+      if (startDates[date] > 0.1 * meetingCount) {
+        finalStartDate = date;
+        break;
+      }
+    }
+
+    // Pick the first day if nothing was decisive.
+    if (!finalStartDate) {
+      macros.log('Warning, no start date was definitive', term.termId, startDates);
+      finalStartDate = startDateKeys[0];
+    }
+
+
+    // Now for the end dates
+    const endDateKeys = _.keys(endDates).sort((a, b) => {
+    // sort in reverse order
+      return parseInt(b, 10) - parseInt(a, 10);
+    });
+
+    for (let i = 0; i < endDateKeys.length; i++) {
+      const date = endDateKeys[i];
+      if (endDates[date] > 0.1 * meetingCount) {
+        finalEndDate = date;
+        break;
+      }
+    }
+
+    // Pick the last day if nothing was decisive.
+    // (the endDateKeys are in reverse chronological order)
+    if (!finalEndDate) {
+      macros.log('Warning, no end date was definitive', term.termId, endDates);
+      finalEndDate = endDateKeys[0];
+    }
+
+
+    term.startDate = finalStartDate;
+    term.endDate = finalEndDate;
+    macros.log(JSON.stringify(term, null, 4));
+    return term;
+  }
+
+
+  go(termDump) {
+    for (const term of termDump.terms) {
+      this.runOnTerm(termDump, term);
+    }
+    return termDump;
+  }
+
 }
-
-TermStartEndDate.prototype = Object.create(BaseProcessor.prototype);
-TermStartEndDate.prototype.constructor = TermStartEndDate;
-
-
-TermStartEndDate.prototype.runOnTerm = function runOnTerm(termDump, term) {
-  const startDates = {};
-  const endDates = {};
-  let meetingCount = 0;
-
-  if (termDump.sections.length === 0) {
-    console.error('No sections in db???', termDump.sections);
-  }
-
-  termDump.sections.forEach((section) => {
-    if (section.termId !== term.termId) {
-      return;
-    }
-
-    if (section.meetings) {
-      section.meetings.forEach((meeting) => {
-        if (startDates[meeting.startDate] === undefined) {
-          startDates[meeting.startDate] = 0;
-        }
-        startDates[meeting.startDate]++;
-
-        if (endDates[meeting.endDate] === undefined) {
-          endDates[meeting.endDate] = 0;
-        }
-        endDates[meeting.endDate]++;
-        meetingCount++;
-      });
-    }
-  });
-
-  let finalStartDate;
-  let finalEndDate;
-
-  const startDateKeys = _.keys(startDates).sort((a, b) => {
-    return parseInt(a, 10) - parseInt(b, 10);
-  });
-
-  for (let i = 0; i < startDateKeys.length; i++) {
-    const date = startDateKeys[i];
-    if (startDates[date] > 0.1 * meetingCount) {
-      finalStartDate = date;
-      break;
-    }
-  }
-
-  // Pick the first day if nothing was decisive.
-  if (!finalStartDate) {
-    macros.log('Warning, no start date was definitive', term.termId, startDates);
-    finalStartDate = startDateKeys[0];
-  }
-
-
-  // Now for the end dates
-  const endDateKeys = _.keys(endDates).sort((a, b) => {
-  // sort in reverse order
-    return parseInt(b, 10) - parseInt(a, 10);
-  });
-
-  for (let i = 0; i < endDateKeys.length; i++) {
-    const date = endDateKeys[i];
-    if (endDates[date] > 0.1 * meetingCount) {
-      finalEndDate = date;
-      break;
-    }
-  }
-
-  // Pick the last day if nothing was decisive.
-  // (the endDateKeys are in reverse chronological order)
-  if (!finalEndDate) {
-    macros.log('Warning, no end date was definitive', term.termId, endDates);
-    finalEndDate = endDateKeys[0];
-  }
-
-
-  term.startDate = finalStartDate;
-  term.endDate = finalEndDate;
-  macros.log(JSON.stringify(term, null, 4));
-  return term;
-};
-
-
-TermStartEndDate.prototype.go = function go(termDump) {
-  for (const term of termDump.terms) {
-    this.runOnTerm(termDump, term);
-  }
-  return termDump;
-};
 
 
 TermStartEndDate.prototype.TermStartEndDate = TermStartEndDate;
-
-module.exports = new TermStartEndDate();
+export default new TermStartEndDate();
 
