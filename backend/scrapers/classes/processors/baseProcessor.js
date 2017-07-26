198f66dff44a914e3e654f7eda4d8d316233c238
Ryan Hughes <ryanhughes624@gmail.com>
2017-07-25 19:55:17 -0700

    .

--- backend/scrapers/classes/processors/baseProcessor.js
+++ backend/scrapers/classes/processors/baseProcessor.js
@@ -13,95 +13,84 @@
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>. 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 import macros from '../../../macros';
-var Keys = require('../../../../common/Keys')
-var path = require('path')
-var fs = require('fs')
+import Keys from '../../../../common/Keys';
 
-function BaseProcessor() {
+class BaseProcessor {
+
+
+  groupSectionsByClass(sections) {
+    const classHash = {};
+
+    sections.forEach((section) => {
+      const obj = {
+        host:section.host,
+        termId: section.termId,
+        subject: section.subject,
+        classUid: section.classUid,
+      };
+
+      const hash = Keys.create(obj).getHash();
+
+      if (!classHash[hash]) {
+        classHash[hash] = [];
+      }
+
+      classHash[hash].push(section);
+    });
+
+    return Object.values(classHash);
+  }
+
+
+  // If config.useClassId, will return {
+  //  'neu.edu201602STAT002':[aClass,aClass]
+  // }
+  // if !config.useClassId, will return {
+  //  'neu.edu201602STAT002_6876877897': aClass
+  // }
+  getClassHash(termDump, config = {}) {
+    // Make obj to find results here quickly.
+    const keyToRows = {};
+
+    termDump.classes.forEach((aClass) => {
+      if (!aClass.host || !aClass.termId || !aClass.subject || !aClass.classUid) {
+        macros.error('ERROR class dosent have required fields??', aClass);
+        return;
+      }
+
+      // multiple classes could have same key
+      let key = aClass.host + aClass.termId + aClass.subject;
+      if (config.useClassId) {
+        key += aClass.classId;
+
+        if (!keyToRows[key]) {
+          keyToRows[key] = [];
+        }
+
+        // only need to keep subject and classUid
+        keyToRows[key].push(aClass);
+      } else if (aClass.classUid) {
+        key += aClass.classUid;
+
+        if (keyToRows[key]) {
+          macros.error('duplicate classUid?', keyToRows[key], aClass);
+        }
+
+        keyToRows[key] = aClass;
+      } else {
+        macros.error('Cant use classUid if dont have classUid!', aClass);
+      }
+    });
+
+    return keyToRows;
+  }
 
 }
-
-
-BaseProcessor.prototype.groupSectionsByClass = function(sections) {
-	var classHash = {};
-
-	sections.forEach(function (section) {
-
-		var obj = {
-			host:section.host,
-			termId: section.termId,
-			subject: section.subject,
-			classUid: section.classUid
-		}
-
-		var hash = Keys.create(obj).getHash();
-
-		if (!classHash[hash]) {
-			classHash[hash] = []
-		}
-
-		classHash[hash].push(section)
-
-	}.bind(this))
-
-	return Object.values(classHash);
-};
-
-
-
-// If config.useClassId, will return {
-// 	'neu.edu201602STAT002':[aClass,aClass]
-// }
-// if !config.useClassId, will return {
-// 	'neu.edu201602STAT002_6876877897': aClass
-// }
-BaseProcessor.prototype.getClassHash = function (termDump, config = {}) {
-
-	// Make obj to find results here quickly.
-	var keyToRows = {};
-
-	termDump.classes.forEach(function (aClass) {
-		if (!aClass.host || !aClass.termId || !aClass.subject || !aClass.classUid) {
-			macros.error("ERROR class dosent have required fields??", aClass);
-			return;
-		}
-
-		// multiple classes could have same key
-		var key = aClass.host + aClass.termId + aClass.subject;
-		if (config.useClassId) {
-			key += aClass.classId
-
-			if (!keyToRows[key]) {
-				keyToRows[key] = []
-			}
-
-			// only need to keep subject and classUid
-			keyToRows[key].push(aClass)
-		}
-		else if (aClass.classUid) {
-			key += aClass.classUid
-
-			if (keyToRows[key]) {
-				macros.error('duplicate classUid?', keyToRows[key], aClass)
-			}
-
-			keyToRows[key] = aClass
-		}
-		else {
-			macros.error('Cant use classUid if dont have classUid!', aClass)
-		}
-
-
-	}.bind(this));
-
-	return keyToRows;
-}
-
 
 
 BaseProcessor.prototype.BaseProcessor = BaseProcessor;
-module.exports = new BaseProcessor()
+export default new BaseProcessor();
