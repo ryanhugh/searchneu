69c5845bf37d3f8d05ae5148a9c87157e7f18853
Ryan Hughes <ryanhughes624@gmail.com>
2017-07-28 20:31:59 -0700

    Bug fix and another attempt at fixing the ram stuff

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -42,7 +42,7 @@ npm run build
 if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
   # npm -g install babel-cli
   cd backend_compiled/scrapers
-  PROD=true NODE_ENV=prod node --max_old_space_size=8192 startup
+  PROD=true NODE_ENV=prod node --max_old_space_size=3072 startup
   cd ../..
   find public
 fi
