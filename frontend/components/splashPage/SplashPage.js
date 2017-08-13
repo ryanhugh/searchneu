66c9844255a687e0a0b907e6694748aa71e87582
Ryan Hughes <ryanhughes624@gmail.com>
2017-08-13 16:08:21 -0400

    .

---
+++ frontend/components/splashPage/SplashPage.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import CSSModules from 'react-css-modules';
+import classNames from 'classnames/bind';
+
+import cs2500Mobile from "./cs2500 mobile.png"
+import cs2500Resultsmobile from "./cs2500 results mobile.png"
+import cs2510Desktop from "./cs2510 desktop.png"
+import engw1111Desktop from "./engw1111 desktop.png"
+import lernerMobile from "./lerner mobile.png"
+import oodMobile1 from "./ood mobile 1.png"
+import oodMobile2 from "./ood mobile 2.png"
+
+import { Grid, Segment, Divider } from 'semantic-ui-react'
+
+import css from './SplashPage.css';
+import macros from '../macros';
+
+const cx = classNames.bind(css);
+
+class SplashPage extends React.Component {
+
+  render () {
+
+    // <Segment className={css.firstContainer}>
+
+        //<div className={css.firstRow}> 
+        //<div className={css.imagesFirstRow}>
+//
+        //</div>
+//
+//
+//
+      //</div>
+
+
+    return  (
+
+      <span>
+  
+        <div className={css.topPadding}></div>
+
+        <Grid>
+          <Grid.Column width={9} className={css.firstRow}>
+            <img className = {css.cs2510Desktop} src={cs2510Desktop}/>
+            <img className = {css.lernerMobile} src={lernerMobile}/>
+          </Grid.Column>
+          <Grid.Column width={7}>
+            <h1> fdja fdja fdsja fd</h1>
+            <div> hi j flkdsaj fldjaf ldsjf dlsaf jldsaj flkdsa jflkdsjf ljfa lsjflkdsajf lkdsjf slaj f</div>
+            <a> Search for CS 2510>></a>
+          </Grid.Column>
+        </Grid>
+      </span>
+
+    )
+  }
+  
+}
+
+
+export default CSSModules(SplashPage, css);
