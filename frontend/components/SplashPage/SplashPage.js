a10a0713d489be90782205abc5b2f5a12af6d229
Ryan Hughes <ryanhughes624@gmail.com>
2017-08-14 01:36:34 -0400

    added second row but its not done

--- frontend/components/SplashPage/SplashPage.js
+++ frontend/components/SplashPage/SplashPage.js
@@ -21,43 +21,48 @@ const cx = classNames.bind(css);
 class SplashPage extends React.Component {
 
   render () {
-
-    // <Segment className={css.firstContainer}>
-
-        //<div className={css.firstRow}> 
-        //<div className={css.imagesFirstRow}> Instantly
-//
-        //</div>
-//
-//
-//
-      //</div>
-
-
     return  (
-
       <span>
-  
         <div className={css.topPadding}></div>
 
-        <Grid>
-          <Grid.Column width={9} className={css.firstRow}>
+        <Grid className={css.firstRow}>
+          {/* These widths must add up to 16.*/}
+          <Grid.Column width={9}>
             <img className = {css.cs2510Desktop} src={cs2510Desktop}/>
             <img className = {css.lernerMobile} src={lernerMobile}/>
           </Grid.Column>
-          <Grid.Column width={7}>
+          <Grid.Column width={7} className={css.rightSideFirstRow}>
             <h1>All of NEU's classes. Instantly Searchable.</h1>
-            <div className={css.firstPanelDesc}>Search for classes, subjects, sections, professors, CRNs, and and more. Instantly find what you are looking for.</div>
-            <a></a>
+            <div className={css.firstPanelDesc}>Easily search through classes, professors, sections, and subjects at Northeastern. Going to add more stuff (like TRACE surveys) soon!</div>
             <Button primary>Search for CS 2510</Button>
           </Grid.Column>
         </Grid>
+
+        <div style={{height:'50px'}}></div>
+
+        <div className='ui divider' />
+
+        <Grid className={css.secondRow}>
+          <Grid.Column width={7} className={css.leftSideSecondRow}>
+            <h1>Everything you could be looking for.</h1>
+            <div className={css.firstPanelDesc}>See class descriptions, Prereqs, Coreqs, CRNs, Professors, Meetings, and Locations!</div>
+            <Button primary>Search for ENGW 1111</Button>
+          </Grid.Column>
+          <Grid.Column width={9} className={css.rightSideFirstRow}>
+            <img className = {css.engw1111Desktop} src={engw1111Desktop}/>
+          </Grid.Column>
+        </Grid>
+
+
       </span>
 
     )
   }
   
 }
+//             <div className={css.firstPanelDesc}>Search for classes, subjects, sections, professors, CRNs, and more. Instantly find what you are looking for.</div>
+
+
 
 
 export default CSSModules(SplashPage, css);
