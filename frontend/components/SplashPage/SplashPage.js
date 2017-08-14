13a473312429f06c8bbec0e8fe49204ea5f6c859
Sean Hughes <shughes@college.harvard.edu>
2017-08-14 16:50:04 -0400

    hi

--- frontend/components/SplashPage/SplashPage.js
+++ frontend/components/SplashPage/SplashPage.js
@@ -16,6 +16,12 @@ import { Button } from 'semantic-ui-react';
 import css from './SplashPage.css';
 import macros from '../macros';
 
+// seanstuff
+import TabPanel from 'react-tab-panel'
+import 'react-tab-panel/index.css'
+
+
+
 const cx = classNames.bind(css);
 
 class SplashPage extends React.Component {
@@ -28,63 +34,23 @@ class SplashPage extends React.Component {
     const searchForLernerEvent = new CustomEvent(macros.searchEvent, { detail: 'Ben Lerner' });
 
     return  (
-      <span>
-        <div className={css.topPadding}></div>
+      <div className = {css.splashPage}>
 
-        <Grid className={css.firstRow}>
-          {/* These widths must add up to 16.*/}
-          <Grid.Column width={9}>
-            <img className = {css.cs2510Desktop} src={cs2510Desktop}/>
-            <img className = {css.lernerMobile} src={lernerMobile}/>
-          </Grid.Column>
-          <Grid.Column width={7} className={css.rightSideFirstRow}>
-            <h1>All of NEU's classes. Instantly Searchable.</h1>
-            <div className={css.firstPanelDesc}>Easily search through classes, professors, sections, and subjects at Northeastern. Going to add more stuff (like TRACE surveys) soon!</div>
-            <Button onClick={() => {window.dispatchEvent(searchForCS2510Event)}} primary>Search for CS 2510</Button>
-          </Grid.Column>
-        </Grid>
+      <TabPanel>
+        <div tabTitle="First Tab">
+          First tab contents here
+        </div>
 
-        <div style={{height:'50px'}}></div>
+        <div tabTitle="Second Tab">
+          Content for second tab here
+        </div>
+      </TabPanel>
 
-        <div className='ui divider' />
-
-        <Grid className={css.secondRow}>
-          <Grid.Column width={7} className={css.leftSideSecondRow}>
-            <h1>Everything you could be looking for.</h1>
-            <div className={css.firstPanelDesc}>See class descriptions, Prereqs, Coreqs, CRNs, Professors, Meetings, and Locations! Going to add even more stuff soon!</div>
-            <Button onClick={() => {window.dispatchEvent(searchForENGW1111Event)}}  primary>Search for ENGW 1111</Button>
-          </Grid.Column>
-          <Grid.Column width={9} className={css.rightSideFirstRow}>
-            <img className = {css.engw1111Desktop} src={engw1111Desktop}/>
-          </Grid.Column>
-        </Grid>
-
-        <div style={{height:'50px'}}></div>
-
-        <div className='ui divider' />
-
-        <div style={{height:'50px'}}></div>
-         <Grid className={css.thirdRow}>
-          <Grid.Column width={9}>
-            <img className = {css.oodMobile1} src={oodMobile1}/>
-            <img className = {css.oodMobile2} src={oodMobile2}/>
-            <img className = {css.cs2500Mobile} src={cs2500Mobile}/>
-            <img className = {css.cs2500Resultsmobile} src={cs2500Resultsmobile}/>
-          </Grid.Column>
-          <Grid.Column width={7} className={css.rightSideFirstRow}>
-            <h1>Works Great on Mobile!</h1>
-            <div className={css.firstPanelDesc}>holla holla</div>
-            <Button onClick={() => {window.dispatchEvent(searchForLernerEvent)}} primary>Search for Ben Lerner</Button>
-          </Grid.Column>
-        </Grid>
-
-        <div style={{height:'50px'}}></div>
-
-      </span>
+      </div>
 
     )
   }
-  
+
 }
 //             <div className={css.firstPanelDesc}>Search for classes, subjects, sections, professors, CRNs, and more. Instantly find what you are looking for.</div>
 
--- frontend/index.js
+++ frontend/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import URI from 'urijs';
 
-
 import macros from './components/macros';
 import Home from './components/Home';
 
@@ -13,25 +12,25 @@ if (window.location.hash === '#notrack') {
   window.localStorage.noTrack = true;
 }
 
-// Segment tracket. This includes trackers for Rollbar and Fullstory.  
-// These are only used on prod and only used if the user has not opted out of tracking. 
+// Segment tracket. This includes trackers for Rollbar and Fullstory.
+// These are only used on prod and only used if the user has not opted out of tracking.
 if (macros.PROD && !window.localStorage.noTrack) {
   !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="4.0.0";
   analytics.load("VWqkionywHZjd2YI3Ds8AE7uLW3DuR19");
   analytics.page();
   }}();
 
-  // Tracker for Google Analytics. 
+  // Tracker for Google Analytics.
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  // Don't log a page view here. A page view will be logged in Home.js when it loads. This will contain info about the search query. 
+  // Don't log a page view here. A page view will be logged in Home.js when it loads. This will contain info about the search query.
   ga('create', 'UA-85376897-3', 'auto');
 
-  // This tracker is from Amplitude. It will queue up calls to window.amplitude before it loaded, and then call when the script loads. 
-  // We could use Amplitude through Segment, but then window.amplitude will not be defined until amplitude.js loads if we did that. 
+  // This tracker is from Amplitude. It will queue up calls to window.amplitude before it loaded, and then call when the script loads.
+  // We could use Amplitude through Segment, but then window.amplitude will not be defined until amplitude.js loads if we did that.
   (function(e,t){var n=e.amplitude||{_q:[],_iq:{}};var r=t.createElement("script");r.type="text/javascript";
   r.async=true;r.src="https://d24n15hnbwhuhn.cloudfront.net/libs/amplitude-3.4.0-min.gz.js";
   r.onload=function(){e.amplitude.runQueuedFunctions()};var i=t.getElementsByTagName("script")[0];
