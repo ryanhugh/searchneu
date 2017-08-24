2aa024c88f418c82ce26c5175aa6823f25d284cb
Ryan Hughes <ryanhughes624@gmail.com>
2017-08-24 11:27:37 -0400

    .

--- backend/scrapers/employees/tests/coe.test.js
+++ backend/scrapers/employees/tests/coe.test.js
@@ -36,28 +36,3 @@ it('should parse q', async (done) => {
   done()
 
 });
-
-
-
-
-// it('should parse all people', async (done) => {
-//   const data = await fs.readFile(path.join(__dirname, 'data', 'ccis', 'view_all_people.json'));
-
-//   const resp = JSON.parse(data);
-//   const output = ccis.parsePeopleList(resp);
-
-//   expect(output).toMatchSnapshot();
-//   done();
-// });
-
-
-// it('parseDetailpage', async (done) => {
-//   const data = await fs.readFile(path.join(__dirname, 'data', 'ccis', 'person.json'));
-//   const resp = JSON.parse(data);
-
-//   const output = ccis.parseDetailpage(resp);
-
-//   expect(output).toMatchSnapshot();
-//   done();
-// });
-
