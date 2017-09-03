9c91cb7bb2c42f5b39ef1d3b794a936efe7e9c66
Ryan Hughes <ryanhughes624@gmail.com>
2017-09-03 16:16:45 -0400

    changed 2 more uas

--- backend/scrapers/login/login.js
+++ backend/scrapers/login/login.js
@@ -337,7 +337,7 @@ async function main() {
 		followRedirect: false,
 		simple: false,
 		headers: {
-			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.A0 Safari/537.36',
+			'User-Agent': ua,
 		}
 	})
 
@@ -359,7 +359,7 @@ async function main() {
 		followRedirect: false,
 		simple: false,
 		headers: {
-			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.A0 Safari/537.36',
+			'User-Agent': ua,
 		}
 	})
 
