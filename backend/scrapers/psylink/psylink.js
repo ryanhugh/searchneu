1e7fba86c6e5459525500ae89bf76f1c69796059
Ryan <rysquash@gmail.com>
2017-09-12 13:25:21 -0400

    "."

---
+++ backend/scrapers/psylink/psylink.js
@@ -0,0 +1,11 @@
+import cheerio from 'cheerio';
+import path from 'path';
+
+import Request from '../request';
+import macros from '../../macros';
+import cache from '../cache';
+
+const request = new Request('psylink');
+
+
+
