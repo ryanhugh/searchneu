ab6136b730cc4494e66383cecf3d60307598beb8
Ryan <rysquash@gmail.com>
2017-09-14 12:02:37 -0400

    "Added a updater.js that might do something someday"

---
+++ backend/scrapers/updater.js
@@ -0,0 +1,17 @@
+import psylink from './psylink/psylink'
+
+// Runs psylink every 5 min. If any now labs are added, send notification
+
+
+class Updater {
+  
+  
+  
+  
+  
+}
+
+
+
+export new Updater()
+
