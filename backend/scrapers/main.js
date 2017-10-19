7540d32f1467c867ca009985eac5510576e9d839
Ryan <rysquash@gmail.com>
2017-10-19 15:51:32 -0400

    "Revered old changes"

--- backend/scrapers/main.js
+++ backend/scrapers/main.js
@@ -14,8 +14,8 @@ import classes from './classes/main';
 
 
 if (process.env.TRAVIS_EVENT_TYPE !== 'cron' && process.env.TRAVIS) {
-  // console.log('not running on travis event', process.env.TRAVIS_EVENT_TYPE);
-  // process.exit(0);
+  console.log('not running on travis event', process.env.TRAVIS_EVENT_TYPE);
+  process.exit(0);
 }
 
 console.log("hi there")
