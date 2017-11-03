eb38492e5a27dcddfcebc94b53e963872b995090
Edward Shen <6173958+edward-shen@users.noreply.github.com>
2017-11-03 17:38:33 -0400

    Tests are now functioning.
    For some reason, the parseClass tests should fail because it should be added to the optional prereqs, but it isn't and it's still being added to the prereqs for class

---
+++ backend/scrapers/classes/processors/tests/addPreRequisiteFor.test.js
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2017 Edward Shen
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+import addPreRequisiteFor from '../addPreRequisiteFor';
+import Keys from '../../../../../common/Keys';
+
+describe('addPreRequisiteFor tests', () => {
+  const cs2500 = {
+    classId: '2500',
+    termId: '201829',
+    host: 'neu.edu',
+  };
+
+  const cs2510 = {
+    prereqs: {
+      type: 'or',
+      values: [{
+        subject: 'CS',
+        classUid: '2500_699845913',
+        classId: '2500',
+      }],
+    },
+    coreqs: {
+      type: 'or',
+      values: [{
+        subject: 'CS',
+        classUid: '2511_803118792',
+        classId: '2511',
+      }],
+    },
+    classId: '2510',
+    termId: '201830',
+    host: 'neu.edu',
+  };
+
+  const termDump = {
+    classes: [cs2500, cs2510],
+    sections: [],
+  };
+
+  it('should load in termDump', () => {
+    addPreRequisiteFor.termDump = termDump;
+    expect(addPreRequisiteFor.termDump).toBe(termDump);
+  });
+
+  describe('parseClass tests', () => {
+    const outputPreReqClass = cs2500;
+    outputPreReqClass.prereqsFor = [];
+    outputPreReqClass.prereqsFor.push(cs2510);
+
+    it('should create a prerequsites field and add the class, if one doesn\'t exist', () => {
+      addPreRequisiteFor.parseClass(cs2510, cs2500, 'and');
+
+      const find = Keys.create(cs2500).getHash();
+      let found = {};
+      for (const aClass of termDump.classes) {
+        if (Keys.create(aClass).getHash() === find) {
+          found = aClass;
+        }
+      }
+      expect(found).toBe(outputPreReqClass);
+    });
+
+    it('should add a new class to an already existing class', () => {
+      addPreRequisiteFor.parseClass(cs2510, cs2500, 'and');
+      outputPreReqClass.prereqsFor.push(cs2510);
+
+      const find = Keys.create(cs2500).getHash();
+      let found = {};
+      for (const aClass of termDump.classes) {
+        if (Keys.create(aClass).getHash() === find) {
+          found = aClass;
+        }
+      }
+      expect(found).toBe(outputPreReqClass);
+    });
+  });
+});
