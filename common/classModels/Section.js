38459166dd976dd217509e22a8a1414df35ea25e
Ryan Hughes <ryanhughes624@gmail.com>
2017-11-15 22:47:28 -0500

    Changed tabs to spaces

--- common/classModels/Section.js
+++ common/classModels/Section.js
@@ -11,18 +11,18 @@ import Meeting from './Meeting';
 
 function Section(config) {
 
-	//loading status is done if any sign that has data
-	if (config.dataStatus !== undefined) {
-		this.dataStatus = config.dataStatus
-	}
-	else if (this.lastUpdateTime !== undefined || this.meetings) {
-		this.dataStatus = macros.DATASTATUS_DONE
-	}
+  //loading status is done if any sign that has data
+  if (config.dataStatus !== undefined) {
+    this.dataStatus = config.dataStatus
+  }
+  else if (this.lastUpdateTime !== undefined || this.meetings) {
+    this.dataStatus = macros.DATASTATUS_DONE
+  }
 
-	//seperate reasons to meet: eg Lecture or Lab.
-	//each of these then has times, and days
-	//instances of Meeting
-	this.meetings = []
+  //seperate reasons to meet: eg Lecture or Lab.
+  //each of these then has times, and days
+  //instances of Meeting
+  this.meetings = []
 }
 
 
@@ -32,275 +32,275 @@ Section.API_ENDPOINT = '/listSections'
 
 
 Section.create = function (config) {
-	var instance = new this(config);
-	instance.updateWithData(config);
-	return instance
+  var instance = new this(config);
+  instance.updateWithData(config);
+  return instance
 }
 
 Section.prototype.meetsOnWeekends = function () {
 
-	for (var i = 0; i < this.meetings.length; i++) {
-		var meeting = this.meetings[i];
+  for (var i = 0; i < this.meetings.length; i++) {
+    var meeting = this.meetings[i];
 
-		if (meeting.getMeetsOnWeekends()) {
-			return true;
-		};
-	}
-	return false;
+    if (meeting.getMeetsOnWeekends()) {
+      return true;
+    };
+  }
+  return false;
 }
 
 Section.prototype.getAllMeetingMoments = function (ignoreExams = true) {
 
-	var retVal = [];
-	this.meetings.forEach(function (meeting) {
-		if (ignoreExams && meeting.getIsExam()) {
-			return;
-		}
+  var retVal = [];
+  this.meetings.forEach(function (meeting) {
+    if (ignoreExams && meeting.getIsExam()) {
+      return;
+    }
 
-		retVal = retVal.concat(_.flatten(meeting.times));
-	}.bind(this))
+    retVal = retVal.concat(_.flatten(meeting.times));
+  }.bind(this))
 
-	retVal.sort(function (a, b) {
-		if (a.start.unix() > b.start.unix()) {
-			return 1;
-		}
-		else if (a.start.unix() < b.start.unix()) {
-			return -1;
-		}
-		else {
-			return 0;
-		}
-	}.bind(this))
+  retVal.sort(function (a, b) {
+    if (a.start.unix() > b.start.unix()) {
+      return 1;
+    }
+    else if (a.start.unix() < b.start.unix()) {
+      return -1;
+    }
+    else {
+      return 0;
+    }
+  }.bind(this))
 
-	return retVal;
+  return retVal;
 };
 
 //returns [false,true,false,true,false,true,false] if meeting mon, wed, fri
 Section.prototype.getWeekDaysAsBooleans = function () {
 
-	var retVal = [false, false, false, false, false, false, false];
+  var retVal = [false, false, false, false, false, false, false];
 
 
-	this.getAllMeetingMoments().forEach(function (time) {
-		retVal[time.start.day()] = true;
-	}.bind(this))
+  this.getAllMeetingMoments().forEach(function (time) {
+    retVal[time.start.day()] = true;
+  }.bind(this))
 
-	return retVal;
+  return retVal;
 };
 
 Section.prototype.getWeekDaysAsStringArray = function () {
 
-	var retVal = [];
+  var retVal = [];
 
-	this.getAllMeetingMoments().forEach(function (time) {
-		var day = time.start.format('dddd');
-		if (retVal.includes(day)) {
-			return;
-		}
-		retVal.push(day);
-	}.bind(this))
+  this.getAllMeetingMoments().forEach(function (time) {
+    var day = time.start.format('dddd');
+    if (retVal.includes(day)) {
+      return;
+    }
+    retVal.push(day);
+  }.bind(this))
 
-	return retVal;
+  return retVal;
 };
 
 //returns true if has exam, else false
 Section.prototype.getHasExam = function () {
-	for (var i = 0; i < this.meetings.length; i++) {
-		if (this.meetings[i].getIsExam()) {
-			return true;
-		}
-	}
-	return false;
+  for (var i = 0; i < this.meetings.length; i++) {
+    if (this.meetings[i].getIsExam()) {
+      return true;
+    }
+  }
+  return false;
 };
 
 //returns the {start:end:} moment object of the first exam found
 //else returns null
 Section.prototype.getExamMeeting = function () {
-	for (var i = 0; i < this.meetings.length; i++) {
-		var meeting = this.meetings[i]
-		if (meeting.getIsExam()) {
-			if (meeting.times.length > 0) {
-				return meeting;
-			};
-		};
-	};
-	return null;
+  for (var i = 0; i < this.meetings.length; i++) {
+    var meeting = this.meetings[i]
+    if (meeting.getIsExam()) {
+      if (meeting.times.length > 0) {
+        return meeting;
+      };
+    };
+  };
+  return null;
 };
 
 // Unique list of all professors in all meetings, sorted alphabetically
 Section.prototype.getProfs = function () {
-	var retVal = [];
-	this.meetings.forEach(function (meeting) {
-		meeting.profs.forEach(function (prof) {
-			if (!retVal.includes(prof)) {
-				retVal.push(prof);
-			};
-		}.bind(this))
-	}.bind(this))
+  var retVal = [];
+  this.meetings.forEach(function (meeting) {
+    meeting.profs.forEach(function (prof) {
+      if (!retVal.includes(prof)) {
+        retVal.push(prof);
+      };
+    }.bind(this))
+  }.bind(this))
 
-	retVal.sort();
+  retVal.sort();
 
-	return retVal;
+  return retVal;
 };
 
 Section.prototype.getLocations = function (ignoreExams) {
-	if (ignoreExams === undefined) {
-		ignoreExams = true;
-	};
+  if (ignoreExams === undefined) {
+    ignoreExams = true;
+  };
 
-	var retVal = [];
-	this.meetings.forEach(function (meeting) {
-		if (ignoreExams && meeting.getIsExam()) {
-			return;
-		};
+  var retVal = [];
+  this.meetings.forEach(function (meeting) {
+    if (ignoreExams && meeting.getIsExam()) {
+      return;
+    };
 
-		var where = meeting.where;
-		if (!retVal.includes(where)) {
-			retVal.push(where);
-		};
-	}.bind(this))
+    var where = meeting.where;
+    if (!retVal.includes(where)) {
+      retVal.push(where);
+    };
+  }.bind(this))
 
-	// If it is at least 1 long with TBAs remove, return the array without any TBAs
-	var noTBAs = _.pull(retVal.slice(0), 'TBA');
-	if (noTBAs.length > 0) {
-		return noTBAs
-	}
-	else {
-		return retVal;
-	}
+  // If it is at least 1 long with TBAs remove, return the array without any TBAs
+  var noTBAs = _.pull(retVal.slice(0), 'TBA');
+  if (noTBAs.length > 0) {
+    return noTBAs
+  }
+  else {
+    return retVal;
+  }
 };
 
 Section.prototype.getUniqueStartTimes = function (ignoreExams) {
-	if (ignoreExams === undefined) {
-		ignoreExams = true;
-	};
+  if (ignoreExams === undefined) {
+    ignoreExams = true;
+  };
 
-	var retVal = [];
+  var retVal = [];
 
-	this.getAllMeetingMoments(ignoreExams).forEach(function (time) {
-		var string = time.start.format('h:mm a');
-		if (!retVal.includes(string)) {
-			retVal.push(string)
-		};
-	}.bind(this))
+  this.getAllMeetingMoments(ignoreExams).forEach(function (time) {
+    var string = time.start.format('h:mm a');
+    if (!retVal.includes(string)) {
+      retVal.push(string)
+    };
+  }.bind(this))
 
-	return retVal;
+  return retVal;
 };
 
 Section.prototype.getUniqueEndTimes = function (ignoreExams) {
-	if (ignoreExams === undefined) {
-		ignoreExams = true;
-	};
+  if (ignoreExams === undefined) {
+    ignoreExams = true;
+  };
 
-	var retVal = [];
+  var retVal = [];
 
-	this.getAllMeetingMoments(ignoreExams).forEach(function (time) {
-		var string = time.end.format('h:mm a');
-		if (!retVal.includes(string)) {
-			retVal.push(string)
-		};
-	}.bind(this))
+  this.getAllMeetingMoments(ignoreExams).forEach(function (time) {
+    var string = time.end.format('h:mm a');
+    if (!retVal.includes(string)) {
+      retVal.push(string)
+    };
+  }.bind(this))
 
-	return retVal;
+  return retVal;
 };
 
 Section.prototype.getHasWaitList = function () {
-	if (this.waitCapacity > 0 || this.waitRemaining > 0) {
-		return true;
-	}
-	else {
-		return false;
-	}
+  if (this.waitCapacity > 0 || this.waitRemaining > 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 
 Section.prototype.updateWithData = function (data) {
-	for (var attrName in data) {
-		if ((typeof data[attrName]) == 'function') {
-			macros.error('given fn??', data, this, this.constructor.name);
-			continue;
-		}
-		this[attrName] = data[attrName]
-	}
+  for (var attrName in data) {
+    if ((typeof data[attrName]) == 'function') {
+      macros.error('given fn??', data, this, this.constructor.name);
+      continue;
+    }
+    this[attrName] = data[attrName]
+  }
 
-	if (data.meetings) {
-		var newMeetings = []
+  if (data.meetings) {
+    var newMeetings = []
 
-		data.meetings.forEach(function (serverData) {
-			newMeetings.push(new Meeting(serverData))
-		}.bind(this))
+    data.meetings.forEach(function (serverData) {
+      newMeetings.push(new Meeting(serverData))
+    }.bind(this))
 
-		this.meetings = newMeetings;
-	}
+    this.meetings = newMeetings;
+  }
 
 };
 
 
 Section.prototype.compareTo = function (other) {
 
-	if (this.online && !other.online) {
-		return 1;
-	}
-	if (other.online && !this.online) {
-		return -1;
-	}
+  if (this.online && !other.online) {
+    return 1;
+  }
+  if (other.online && !this.online) {
+    return -1;
+  }
 
-	if (this.meetings.length == 0 && other.meetings.length === 0) {
-		return 0;
-	}
-	if (this.meetings.length > 0 && other.meetings.length == 0) {
-		return -1
-	}
-	else if (this.meetings.length === 0 && other.meetings.length > 0) {
-		return 1
-	}
+  if (this.meetings.length == 0 && other.meetings.length === 0) {
+    return 0;
+  }
+  if (this.meetings.length > 0 && other.meetings.length == 0) {
+    return -1
+  }
+  else if (this.meetings.length === 0 && other.meetings.length > 0) {
+    return 1
+  }
 
-	// If both sections have meetings, then sort alphabetically by professor.
-	const thisProfs = this.getProfs()
-	const otherProfs = other.getProfs()
-	const thisOnlyTBA = thisProfs.length === 1 && thisProfs[0] === 'TBA';
-	const otherOnlyTBA = otherProfs.length === 1 && otherProfs[0] === 'TBA';
+  // If both sections have meetings, then sort alphabetically by professor.
+  const thisProfs = this.getProfs()
+  const otherProfs = other.getProfs()
+  const thisOnlyTBA = thisProfs.length === 1 && thisProfs[0] === 'TBA';
+  const otherOnlyTBA = otherProfs.length === 1 && otherProfs[0] === 'TBA';
 
-	if (thisProfs.length > 0 || otherProfs.length > 0) {
+  if (thisProfs.length > 0 || otherProfs.length > 0) {
 
-		if (thisProfs.length === 0) {
-			return -1;
-		}
-		if (otherProfs.length === 0) {
-			return 1
-		}
+    if (thisProfs.length === 0) {
+      return -1;
+    }
+    if (otherProfs.length === 0) {
+      return 1
+    }
 
-		if (thisOnlyTBA && !otherOnlyTBA) {
-			return 1;
-		}
-		if (!thisOnlyTBA && otherOnlyTBA) {
-			return -1;
-		}
+    if (thisOnlyTBA && !otherOnlyTBA) {
+      return 1;
+    }
+    if (!thisOnlyTBA && otherOnlyTBA) {
+      return -1;
+    }
 
-		if (thisProfs[0] > otherProfs[0]) {
-			return 1;
-		}
-		if (otherProfs[0] > thisProfs[0]) {
-			return -1;
-		}
-	}
+    if (thisProfs[0] > otherProfs[0]) {
+      return 1;
+    }
+    if (otherProfs[0] > thisProfs[0]) {
+      return -1;
+    }
+  }
 
-	// Then, sort by the starting time of the section.
-	if (this.meetings[0].times.length === 0) {
-		return 1;
-	}
-	if (other.meetings[0].times.length === 0) {
-		return -1;
-	}
-	if (this.meetings[0].times[0][0].start.unix() < other.meetings[0].times[0][0].start.unix()) {
-		return -1;
-	}
-	if (this.meetings[0].times[0][0].start.unix() > other.meetings[0].times[0][0].start.unix()) {
-		return 1;
-	}
-	else {
-		return 0;
-	}
+  // Then, sort by the starting time of the section.
+  if (this.meetings[0].times.length === 0) {
+    return 1;
+  }
+  if (other.meetings[0].times.length === 0) {
+    return -1;
+  }
+  if (this.meetings[0].times[0][0].start.unix() < other.meetings[0].times[0][0].start.unix()) {
+    return -1;
+  }
+  if (this.meetings[0].times[0][0].start.unix() > other.meetings[0].times[0][0].start.unix()) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
 };
 
 
