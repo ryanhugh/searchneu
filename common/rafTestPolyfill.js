af5978a7348f54949fdf832713564e3946192529
Ryan Hughes <ryanhughes624@gmail.com>
2017-11-17 10:47:13 -0500

    Fixed linting errors with raf polyfill

--- common/rafTestPolyfill.js
+++ common/rafTestPolyfill.js
@@ -2,7 +2,7 @@
 // Fix to hide this warning until jsdom adds support for requestAnimationFrame
 // https://github.com/facebook/jest/issues/4545
 if (process.env.NODE_ENV === 'test') {
-  global.requestAnimationFrame = function (callback) {
-    setTimeout(callback, 0)
-  }
+  global.requestAnimationFrame = (callback) => {
+    setTimeout(callback, 0);
+  };
 }
