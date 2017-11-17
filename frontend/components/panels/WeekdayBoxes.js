33d2b98e14ae3edb72bd56a733bfe11c4ac133b3
Ryan Hughes <ryanhughes624@gmail.com>
2017-11-17 11:19:58 -0500

    Made some eslint fixes in weekdayboxes.js

--- frontend/components/panels/WeekdayBoxes.js
+++ frontend/components/panels/WeekdayBoxes.js
@@ -14,48 +14,46 @@ const cx = classNames.bind(css);
 // This file is responsible for the weekday boxes, eg [][x][][][x] for Tuesday Friday
 // And the string that appears when you hover over it (Meets on Tuesday, Friday)
 
-class WeekdayBoxes extends React.Component {
-  render() {
-    // Don't render anything if the class is online.
-    if (this.props.section.online) {
-      return null;
-    }
-
-
-    // Calculate the "Meets on Tuesday, Friday" or "No Meetings found" string that hovers over the weekday boxes
-    const meetingDays = this.props.section.getWeekDaysAsStringArray();
-    let meetingString;
-    if (meetingDays.length === 0) {
-      meetingString = 'No Meetings found';
-    } else {
-      meetingString = `Meets on ${meetingDays.join(', ')}`;
-    }
-
-    // Calculate the weekday boxes eg [][x][][][x] for Tuesday Friday
-    let booleans = this.props.section.getWeekDaysAsBooleans();
-    if (!this.props.section.meetsOnWeekends()) {
-      booleans = booleans.slice(1, 6);
-    }
-
-    const booleanElements = booleans.map((meets, index) => {
-      return (
-        <div
-          key={ index }
-          className={ cx({
-            weekDayBoxChecked: meets,
-            weekDayBox: true,
-          }) }
-        />
-      );
-    });
-
-
-    return (
-      <div className={ `${css.inlineBlock} ${css.daysContainer}` } data-tip={ meetingString }>
-        {booleanElements}
-      </div>
-    );
+function WeekdayBoxes(props) {
+  // Don't render anything if the class is online.
+  if (props.section.online) {
+    return null;
   }
+
+
+  // Calculate the "Meets on Tuesday, Friday" or "No Meetings found" string that hovers over the weekday boxes
+  const meetingDays = props.section.getWeekDaysAsStringArray();
+  let meetingString;
+  if (meetingDays.length === 0) {
+    meetingString = 'No Meetings found';
+  } else {
+    meetingString = `Meets on ${meetingDays.join(', ')}`;
+  }
+
+  // Calculate the weekday boxes eg [][x][][][x] for Tuesday Friday
+  let booleans = props.section.getWeekDaysAsBooleans();
+  if (!props.section.meetsOnWeekends()) {
+    booleans = booleans.slice(1, 6);
+  }
+
+  const booleanElements = booleans.map((meets, index) => {
+    return (
+      <div
+        key={ index } // eslint-disable-line react/no-array-index-key
+        className={ cx({
+          weekDayBoxChecked: meets,
+          weekDayBox: true,
+        }) }
+      />
+    );
+  });
+
+
+  return (
+    <div className={ `${css.inlineBlock} ${css.daysContainer}` } data-tip={ meetingString }>
+      {booleanElements}
+    </div>
+  );
 }
 
 
