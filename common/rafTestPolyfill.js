8093ce6a46abe17c959b55c10e1f55a4fe0d8d88
Ryan Hughes <ryanhughes624@gmail.com>
2017-11-17 02:22:02 -0500

    Added requestAnimationFrame polyfill

---
+++ common/rafTestPolyfill.js
@@ -0,0 +1,8 @@
+
+// Fix to hide this warning until jsdom adds support for requestAnimationFrame
+// https://github.com/facebook/jest/issues/4545
+if (process.env.NODE_ENV === 'test') {
+  global.requestAnimationFrame = function (callback) {
+    setTimeout(callback, 0)
+  }
+}
