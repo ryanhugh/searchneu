7c6203cc31ca56344f0c300ffa7e8b1b9bc78090
Ryan Hughes <ryanhughes624@gmail.com>
2017-11-17 11:41:23 -0500

    Everything passes the linting now! and added the eslint to the webpack, not totally sure if this is somehting we watnt to stick with

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
     'prefer-destructuring': 0,
     'jsx-a11y/click-events-have-key-events': 0,
     'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'jsx-a11y/anchor-is-valid': 0,
 
     // Allow for-of loops. 
     "no-restricted-syntax": [
