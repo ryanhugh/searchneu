bdfaf5e1c1e18b5de356b8c72e47a65463f18769
Edward Shen <6173958+edward-shen@users.noreply.github.com>
2017-11-21 10:48:01 -0500

    eslinted code

--- frontend/components/panels/tests/BaseClassPanel.test.js
+++ frontend/components/panels/tests/BaseClassPanel.test.js
@@ -11,7 +11,7 @@ import BaseClassPanel from '../BaseClassPanel';
 
 
 it('should not render a show more button', () => {
-  const component = renderer.create(<BaseClassPanel aClass={mockData.cs0210} />);
+  const component = renderer.create(<BaseClassPanel aClass={ mockData.cs0210 } />);
 
   const button = component.getInstance().getShowMoreButton();
 
@@ -20,9 +20,9 @@ it('should not render a show more button', () => {
 
 
 it('should render a button', () => {
-  const component = renderer.create(<BaseClassPanel aClass={mockData.cs1210} />);
+  const component = renderer.create(<BaseClassPanel aClass={ mockData.cs1210 } />);
 
-  let tree = component.toJSON();
+  // const tree = component.toJSON();
 
   const button = component.getInstance().getShowMoreButton();
 
@@ -30,51 +30,50 @@ it('should render a button', () => {
 });
 
 it('clicking the button will cause more sections to be rendered', () => {
-  const component = renderer.create(<BaseClassPanel aClass={mockData.cs1210} />);
+  const component = renderer.create(<BaseClassPanel aClass={ mockData.cs1210 } />);
 
-  expect(component.getInstance().state.renderedSections.length).toBe(3)
-  expect(component.getInstance().state.unrenderedSections.length).toBe(2)
+  expect(component.getInstance().state.renderedSections.length).toBe(3);
+  expect(component.getInstance().state.unrenderedSections.length).toBe(2);
 
-  component.getInstance().onShowMoreClick()
+  component.getInstance().onShowMoreClick();
 
-  expect(component.getInstance().state.renderedSections.length).toBe(5)
-  expect(component.getInstance().state.unrenderedSections.length).toBe(0)
+  expect(component.getInstance().state.renderedSections.length).toBe(5);
+  expect(component.getInstance().state.unrenderedSections.length).toBe(0);
 });
 
 
-it('should display credits string', function() {
-  const component = renderer.create(<BaseClassPanel aClass={mockData.cs1210} />);
+it('should display credits string', () => {
+  const component = renderer.create(<BaseClassPanel aClass={ mockData.cs1210 } />);
 
-  let instance = component.getInstance()
+  let instance = component.getInstance();
 
-  expect(instance.getCreditsString()).toBe('1 credits')
+  expect(instance.getCreditsString()).toBe('1 credits');
 
-  const cs0210 = renderer.create(<BaseClassPanel aClass={mockData.cs0210} />);
+  const cs0210 = renderer.create(<BaseClassPanel aClass={ mockData.cs0210 } />);
 
-  instance = cs0210.getInstance()
+  instance = cs0210.getInstance();
 
-  expect(instance.getCreditsString()).toBe('1 to 49 credits')
-
+  expect(instance.getCreditsString()).toBe('1 to 49 credits');
 });
 
 
-it('should render prereqs string', function() {
-  const component = renderer.create(<BaseClassPanel aClass={mockData.cs0210} />);
+it('should render prereqs string', () => {
+  const component = renderer.create(<BaseClassPanel aClass={ mockData.cs0210 } />);
 
-  let instance = component.getInstance()
+  const instance = component.getInstance();
 
-  let prereqs = instance.getReqsString('prereqs')
+  const prereqs = instance.getReqsString('prereqs');
 
-  expect(prereqs).toMatchSnapshot()
+  expect(prereqs).toMatchSnapshot();
 });
 
 
-it('should render coreqs string', function() {
-  const component = renderer.create(<BaseClassPanel aClass={mockData.cs0210} />);
+it('should render coreqs string', () => {
+  const component = renderer.create(<BaseClassPanel aClass={ mockData.cs0210 } />);
 
-  let instance = component.getInstance()
+  const instance = component.getInstance();
 
-  let coreqs = instance.getReqsString('coreqs')
+  const coreqs = instance.getReqsString('coreqs');
 
-  expect(coreqs).toMatchSnapshot()
+  expect(coreqs).toMatchSnapshot();
 });
