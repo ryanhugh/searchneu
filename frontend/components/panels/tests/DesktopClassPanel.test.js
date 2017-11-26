2e88a14385644ecb749c228d0213a642018fa7d8
Ryan Hughes <ryanhughes624@gmail.com>
2017-11-26 11:19:40 -0500

    .

--- frontend/components/panels/tests/DesktopClassPanel.test.js
+++ frontend/components/panels/tests/DesktopClassPanel.test.js
@@ -6,9 +6,13 @@
 import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
 import MockDate from 'mockdate';
+import Enzyme, { shallow, render, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 import mockData from './mockData';
 import DesktopClassPanel from '../DesktopClassPanel';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 beforeAll(() => {
   MockDate.set('Mon Nov 26 2017 00:00:00 -0000');
@@ -26,3 +30,19 @@ it('should render some stuff', () => {
   expect(result).toMatchSnapshot();
 });
 
+it('shouldShowWaitlist should work', () => {
+
+  let componet = <DesktopClassPanel aClass={ mockData.cs1210 } />
+
+  const wrapper = shallow(componet);
+
+  let instance = wrapper.instance();
+
+  console.log(instance.shouldShowWaitlist())
+
+  // console.log(instance.shouldShowWaitlist, 'here')
+
+    expect(wrapper).toMatchSnapshot();
+
+    
+})
