e1f9fc0e7bf56e9ee65d8c858756da3e45b0105d
Ryan Hughes <ryanhughes624@gmail.com>
2017-11-25 19:20:41 -0500

    Changed to shallow rendering

--- frontend/components/panels/tests/DesktopClassPanel.test.js
+++ frontend/components/panels/tests/DesktopClassPanel.test.js
@@ -4,17 +4,18 @@
  */
 
 import React from 'react';
-import renderer from 'react-test-renderer';
 import ShallowRenderer from 'react-test-renderer/shallow';
-
 
 import mockData from './mockData';
 import DesktopClassPanel from '../DesktopClassPanel';
 
-
 it('should render some stuff', () => {
 
-  const component = renderer.create(<DesktopClassPanel aClass={mockData.cs1210} />);
-  expect(component).toMatchSnapshot();
+  const renderer = new ShallowRenderer();
+  renderer.render(<DesktopClassPanel aClass={ mockData.cs1210 } />);
+  const result = renderer.getRenderOutput();
+
+  expect(result).toMatchSnapshot();
 
 });
+
