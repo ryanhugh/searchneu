f92f4265de4b4481e035568bb8322e736b7a1cde
Ryan Hughes <ryanhughes624@gmail.com>
2017-11-27 17:33:37 -0500

    Added a bunch more tests

--- frontend/components/panels/tests/LocationLinks.test.js
+++ frontend/components/panels/tests/LocationLinks.test.js
@@ -7,18 +7,21 @@ import React from 'react';
 
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import macros from '../../macros';
 
 import mockData from './mockData';
-import DesktopSectionPanel from '../DesktopSectionPanel';
+import LocationLinks from '../LocationLinks';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+
 it('should render a section', () => {
-  const result = shallow(<DesktopSectionPanel shouldShowExamColumns={ false } showWaitList={ false } section={ mockData.cs1210.sections[0] } />);
+  const result = shallow(<LocationLinks section={ mockData.cs1210.sections[0] } />);
   expect(result).toMatchSnapshot();
 });
 
+
 it('should render another section', () => {
-  const result = shallow(<DesktopSectionPanel shouldShowExamColumns={ false } showWaitList={ false } section={ mockData.cs1210.sections[1] } />);
+  const result = shallow(<LocationLinks section={ mockData.cs0210.sections[0] } />);
   expect(result).toMatchSnapshot();
 });
