d21f5a046ef75ad410c70fcc86a9031f074bd874
Ryan Hughes <ryanhughes624@gmail.com>
2017-11-27 11:25:49 -0500

    Added a file to generate a sitemap with urls to everyyyyything

---
+++ backend/scrapers/sitemapGenerator.js
@@ -0,0 +1,68 @@
+/*
+ * This file is part of Search NEU and licensed under AGPL3.
+ * See the license file in the root folder for details.
+ */
+
+import fs from 'fs-promise';
+import path from 'path';
+import macros from '../macros';
+
+class SitemapGenerator {
+
+
+  async go(termDump, mergedEmployees) {
+
+    // Items to link to.
+    // The part after the https://searchneu.com/
+    let items = [];
+
+    // Add the subjects
+    for (const subject of termDump.subjects) {
+
+      if (subject.termId !== '201830') {
+        continue;
+      }
+
+      items.push(subject.subject)
+      items.push(subject.text)
+    }
+
+    // Add the classes
+    for (const aClass of termDump.classes) {
+
+      if (aClass.termId !== '201830') {
+        continue;
+      }
+
+      items.push(aClass.subject +' '+aClass.classId)
+      items.push(aClass.name)
+    }
+
+    // Add the employees
+    for (const employee of mergedEmployees) {
+      items.push(employee.name)
+    }
+
+    // Convert the items to urls and put them inside xml
+    let xml = ['<?xml version="1.0" encoding="UTF-8"?>','<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'];
+    for (const item of items) {
+      xml.push('  <url>')
+      xml.push('    <loc>https://searchneu.com/' + encodeURIComponent(item) + '</loc>')
+      xml.push('  </url>')
+    }
+    xml.push('</urlset>')
+
+    let output = xml.join('\n')
+
+    await fs.writeFile(path.join('public', 'sitemap.xml'), output);
+
+  }
+
+
+
+
+
+}
+
+
+export default new SitemapGenerator();
