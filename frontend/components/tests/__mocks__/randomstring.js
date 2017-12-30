aa068d6717188cf3780128ce6a80d342d1cb735d
Ryan Hughes <ryanhughes624@gmail.com>
2017-12-30 02:16:45 -0500

    Added more tests for Sign up for notifications and the auth component

--- frontend/components/tests/__mocks__/randomstring.js
+++ frontend/components/tests/__mocks__/randomstring.js
@@ -3,15 +3,15 @@
  * See the license file in the root folder for details.
  */
 
- // This file is responsible for mocking out the randomstring module.
- // This will always return the same string for a given input.
+// This file is responsible for mocking out the randomstring module.
+// This will always return the same string for a given input.
 
 export default {
   generate: (length) => {
     let output = '';
     while (output.length < length) {
-      output += '0'
+      output += '0';
     }
     return output;
-  }
-}
+  },
+};
