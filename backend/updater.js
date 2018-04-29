52398f403c61d08c1f6a945df1445dba04af9834
Ryan Hughes <ryanhughes624@gmail.com>
2018-04-29 16:26:46 +0800

    fixed a bug with the updater

--- backend/updater.js
+++ backend/updater.js
@@ -9,6 +9,7 @@ import DataLib from '../common/classModels/DataLib';
 
 import classesScrapers from './scrapers/classes/main';
 
+import _ from 'lodash';
 import macros from './macros';
 import database from './database';
 import Keys from '../common/Keys';
@@ -67,8 +68,8 @@ class Updater {
     const classHashToUsers = {};
 
     for (const user of users) {
-      classHashes = user.watchingClasses.concat(classHashes);
-      sectionHashes = user.watchingSections.concat(sectionHashes);
+      classHashes = _.uniq(user.watchingClasses).concat(classHashes);
+      sectionHashes = _.uniq(user.watchingSections).concat(sectionHashes);
 
       for (const classHash of user.watchingClasses) {
         if (!classHashToUsers[classHash]) {
@@ -244,39 +245,36 @@ class Updater {
       }
     }
 
-    if (output.sections) {
-      for (const newSection of output.sections) {
-        const hash = Keys.create(newSection).getHash();
+    for (const newSection of output.sections) {
+      const hash = Keys.create(newSection).getHash();
 
-        const oldSection = this.dataLib.getSectionServerDataFromHash(hash);
+      const oldSection = this.dataLib.getSectionServerDataFromHash(hash);
 
-        // This should never run.
-        // The user should not be able to sign up for a section that didn't exist when they were signing up.
-        if (!oldSection) {
-          macros.error('Section was added?', hash);
-          continue;
+      // This should never run.
+      // The user should not be able to sign up for a section that didn't exist when they were signing up.
+      if (!oldSection) {
+        macros.error('Section was added?', hash);
+        continue;
+      }
+
+      let message;
+
+      if (newSection.seatsRemaining > 0 && oldSection.seatsRemaining <= 0) {
+        // See above comment about space before the exclamation mark.
+        message = `A seat opened up in ${newSection.subject} ${newSection.classId} (CRN: ${newSection.crn}). Check it out at https://searchneu.com/${newSection.subject}${newSection.classId} !`;
+      } else if (newSection.waitRemaining > 0 && oldSection.waitRemaining <= 0) {
+        message = `A waitlist seat opened up in ${newSection.subject} ${newSection.classId} (CRN: ${newSection.crn}). Check it out at https://searchneu.com/${newSection.subject}${newSection.classId} !`;
+      }
+
+      if (message) {
+        const user = sectionHashToUsers[hash];
+        if (!userToMessageMap[user]) {
+          userToMessageMap[user] = [];
         }
 
-        let message;
-
-        if (newSection.seatsRemaining > 0 && oldSection.seatsRemaining <= 0) {
-          // See above comment about space before the exclamation mark.
-          message = `A seat opened up in ${newSection.subject} ${newSection.classId} (CRN: ${newSection.crn}). Check it out at https://searchneu.com/${newSection.subject}${newSection.classId} !`;
-        } else if (newSection.waitRemaining > 0 && oldSection.waitRemaining <= 0) {
-          message = `A waitlist seat opened up in ${newSection.subject} ${newSection.classId} (CRN: ${newSection.crn}). Check it out at https://searchneu.com/${newSection.subject}${newSection.classId} !`;
-        }
-
-        if (message) {
-          const user = sectionHashToUsers[hash];
-          if (!userToMessageMap[user]) {
-            userToMessageMap[user] = [];
-          }
-
-          userToMessageMap[user].push(message);
-        }
+        userToMessageMap[user].push(message);
       }
     }
-
 
     // Loop through the messages and send them.
     for (const fbUserId of Object.keys(userToMessageMap)) {
