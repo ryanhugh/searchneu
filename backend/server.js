6d7cc1406da096bbda5a82fb4cc24575208dab65
Ryan Hughes <ryanhughes624@gmail.com>
2018-05-13 18:53:33 +0800

    removed refferals for now

--- backend/server.js
+++ backend/server.js
@@ -493,7 +493,6 @@ async function onSendToMessengerButtonClick(sender, userPageId, b64ref) {
       facebookMessengerId: sender,
       facebookPageId: userPageId,
       loginKeys: [userObject.loginKey],
-      referrals: 0,
     };
 
     macros.log('Adding ', newUser, 'to the db');
