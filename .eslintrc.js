ea670ce54c20df19c806d8541338d883c52e1efc
Ryan Hughes <rysquash@gmail.com>
2018-08-05 10:05:06 -0700

    ..

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     'jsx-a11y/click-events-have-key-events': 0,
     'jsx-a11y/no-noninteractive-element-interactions': 0,
     'jsx-a11y/anchor-is-valid': 0,
+    'jsx-a11y/label-has-for': 0,
 
     // Allow for-of loops. 
     "no-restricted-syntax": [
