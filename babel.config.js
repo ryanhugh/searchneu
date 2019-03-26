1ff04ac6eda284ecf0578fc9f8ea746024176a22
Ryan Hughes <ryanhughes624@gmail.com>
2019-03-26 13:52:13 -0400

    fixed a css file formatting

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 // This empty file is used to tell babel-node that this is the root directory of this project
-// If babel-node is ran from a subdirectory, it will look upward (if the "upward command is ran") for a folder
+// If babel-node is ran from a subdirectory, it will look upward (if in "upward" mode) for a folder
 // containing babel.config.js
 // This is new in babel 7
 // The babel config is in package.json
