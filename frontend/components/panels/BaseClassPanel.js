3944715a6f13629b2196a8e53a2f202889f114f5
Ryan Hughes <ryanhughes624@gmail.com>
2019-04-03 14:20:03 -0400

    Renamed courseAttributes to classAttributes

--- frontend/components/panels/BaseClassPanel.js
+++ frontend/components/panels/BaseClassPanel.js
@@ -126,8 +126,8 @@ class BaseClassPanel extends React.Component {
 
   // returns course Attributes
   // -> List
-  getCourseAttributesString() {
-    return this.props.aClass.courseAttributes;
+  getClassAttributesString() {
+    return this.props.aClass.classAttributes;
   }
 
   // returns an array made to be rendered by react to display the prereqs
