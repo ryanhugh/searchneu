dcf21e0333daf94924c89b2587e37d397878496f
Ryan Hughes <ryanhughes624@gmail.com>
2019-05-21 11:55:32 -0400

    some more random things from chrome auditor

--- frontend/index.js
+++ frontend/index.js
@@ -58,10 +58,8 @@ if (macros.PROD && !window.localStorage.noTrack) {
   }
 
   // Tracker for Google Analytics.
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  // The HTML part for this tracker is in index.html
+  window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
 
   // Don't log a page view here. A page view will be logged in Home.js when it loads. This will contain info about the search query.
   ga('create', macros.googleAnalyticsToken, 'auto');
