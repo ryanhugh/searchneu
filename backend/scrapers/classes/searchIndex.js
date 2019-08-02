b38bd03af15a0534d4d48c430e050b9ad2fd100d
Da-Jin Chu <dchu@alignable.com>
2019-08-01 23:43:20 -0400

    searchIndex puts all into same index

--- backend/scrapers/classes/searchIndex.js
+++ backend/scrapers/classes/searchIndex.js
@@ -12,60 +12,21 @@ import Elastic from '../../elastic';
 
 
 class SearchIndex {
-  // Class Lists object is specific to this file, and is created below.
-  async createSearchIndexFromClassLists(termData) {
-    const bulk = [];
-    for (const attrName2 of Object.keys(termData.classHash)) {
-      const searchResultData = termData.classHash[attrName2];
-
-      // Add a code attribute (CS2500) and tokenize it using word_delimiter
-      // This allows "cs2500" and "cs 2500" to both find class.code correctly.
-      const clas = searchResultData.class;
-      clas.code = clas.subject + clas.classId;
-      searchResultData.type = 'class';
-
-      bulk.push({ index:{ _id: attrName2 } });
-      bulk.push(searchResultData);
-    }
-    
-    await Elastic.resetIndex(`term${termData.termId}`, mapping);
-    await Elastic.bulk({ index: `term${termData.termId}`, body: bulk });
-    macros.log('indexed ', termData.termId);
-  }
-
-
-  async createSearchIndex(termDump) {
-    const classLists = {};
+  attachSectionsToClasses(termDump) {
+    // Hashmap of all classes. Used to more quickly lookup classes
+    const classMap = {};
 
     termDump.classes.forEach((aClass) => {
-      const termHash = Keys.getTermHash({
-        host: aClass.host,
-        termId: aClass.termId,
-      });
-
       const classHash = Keys.getClassHash(aClass);
 
-      if (!classLists[termHash]) {
-        classLists[termHash] = {
-          classHash: {},
-          host: aClass.host,
-          termId: aClass.termId,
-        };
-      }
-
-      classLists[termHash].classHash[classHash] = {
+      classMap[classHash] = {
         class: aClass,
         sections: [],
       };
     });
 
-
+    // Loop through all sections and associate them with the right class
     termDump.sections.forEach((section) => {
-      const termHash = Keys.getTermHash({
-        host: section.host,
-        termId: section.termId,
-      });
-
       const classHash = Keys.getClassHash({
         host: section.host,
         termId: section.termId,
@@ -73,46 +34,44 @@ class SearchIndex {
         classId: section.classId,
       });
 
-
-      if (!classLists[termHash]) {
-        // The objects should all have been created when looping over the classes.
-        macros.error('Dont have obj in section for loop?', termHash, classHash, section);
-        return;
-      }
-
-      if (!classLists[termHash].classHash[classHash]) {
+      if (!classMap[classHash]) {
         // This should never happen now that the bug has been fixed.
         macros.error('No class exists with same data?', classHash, section.url);
         return;
       }
 
-      classLists[termHash].classHash[classHash].sections.push(section);
+      classMap[classHash].sections.push(section);
     });
-
-    // Sort each classes section by crn.
-    // This will keep the sections the same between different scrapings.
-    const termHashes = Object.keys(classLists);
-    for (const termHash of termHashes) {
-      const classHashes = Object.keys(classLists[termHash].classHash);
-      for (const classHash of classHashes) {
-        if (classLists[termHash].classHash[classHash].sections.length > 1) {
-          classLists[termHash].classHash[classHash].sections.sort((a, b) => {
-            return a.crn > b.crn;
-          });
-        }
-      }
-    }
-
-    const promises = [];
-
-    for (const attrName of Object.keys(classLists)) {
-      const termData = classLists[attrName];
-      promises.push(this.createSearchIndexFromClassLists(termData));
-    }
-
-    return Promise.all(promises);
+    return classMap;
   }
 
+  async createSearchIndex(termDump) {
+    const classes = this.attachSectionsToClasses(termDump);
+
+    const bulk = [];
+    for (const classHash of Object.keys(classes)) {
+      const clas = classes[classHash];
+      // Sort each classes section by crn.
+      // This will keep the sections the same between different scrapings.
+      if (clas.sections.length > 1) {
+        clas.sections.sort((a, b) => {
+          return a.crn > b.crn;
+        });
+      }
+
+      // Add a code attribute (CS2500) and tokenize it using word_delimiter
+      // This allows "cs2500" and "cs 2500" to both find class.code correctly.
+      clas.code = clas.subject + clas.classId;
+      clas.type = 'class';
+
+      bulk.push({ index: { _id: classHash } });
+      bulk.push(clas);
+    }
+    await Elastic.resetIndex('classes', mapping);
+    macros.log('performing bulk insert to index classes');
+    await Elastic.bulk({ index: 'classes', body: bulk });
+    macros.log('indexed classes');
+  }
 
   async main(termDump) {
     if (!termDump) {
