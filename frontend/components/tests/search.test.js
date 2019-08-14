49ba4e06eb57c6331223245f35873c5cc07c876e
Ryan Hughes <ryanhughes624@gmail.com>
2019-08-13 23:04:50 -0600

    more

--- frontend/components/tests/search.test.js
+++ frontend/components/tests/search.test.js
@@ -9,22 +9,27 @@ import search from '../search';
 import requestMock from '../__mocks__/request';
 
 
-// Importing this one will get the mocked out instance above. 
+// Importing this one will get the mocked out instance above.
 // If you directly import '../__mocks__/request' you will get a different instance of requestMock
 import requestmock2 from '../request'
 
 
-// beforeAll(() => {
-//   MockDate.set('Mon Oct 10 2016 00:00:00 -0000');
-// });
-
+// Make sure to reset the mock request module after each test. 
 afterAll(() => {
   requestmock2.reset();
 });
 
 
-it('should do nothing', async (done) => {
+it('should not have any results', async (done) => {
 	requestmock2.setBenResponse(false);
+  let results = await search.search('ben', '201850', 4);
+
+  expect(results.length).toBe(0);
+  done();
+})
+
+
+it('should do nothing', async (done) => {
   let results = await search.search('ben', '201850', 4);
 
   console.log(results.results.length)
