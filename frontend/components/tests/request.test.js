71889db47ed10681cb130425e3c3d29b8ca6ff81
Ryan Hughes <ryanhughes624@gmail.com>
2019-08-14 16:40:35 -0600

    removed two unused packages

---
+++ frontend/components/tests/request.test.js
@@ -0,0 +1,19 @@
+/*
+ * This file is part of Search NEU and licensed under AGPL3.
+ * See the license file in the root folder for details.
+ */
+
+import _ from 'lodash';
+import request from '../request';
+
+
+// Make sure to reset the mock request module after each test.
+afterEach(() => {
+  
+  
+});
+
+beforeEach(() => {
+  
+  
+});
