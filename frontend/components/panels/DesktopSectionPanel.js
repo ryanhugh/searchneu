76be40831c83fd905f24f5ef2e76f16e7ff01404
NEUDitao <li.edwa@husky.neu.edu>
2019-08-17 11:47:04 -0700

    changed iconography of full classes, but correctly this time

--- frontend/components/panels/DesktopSectionPanel.js
+++ frontend/components/panels/DesktopSectionPanel.js
@@ -137,12 +137,12 @@ export default class DesktopSectionPanel extends React.Component {
 
       let notifBox;
 
-      if (this.props.seatsRemaining > 0) {
-	      notifBox = <div data-tip='Sign up for notifications for this class' className='inlineBlock'>
+      if (this.props.section.seatsRemaining == 0) {
+	      notifBox = <div data-tip='Sign up for notifications for this section' className='inlineBlock'>
               <NotifCheckBox seats={ this.props.section.seatsRemaining } section={ this.props.section } />
               </div>
       } else {
-	  notifBox = <div data-tip='There are still seats remaining for this class' className='inlineBlock'>
+	  notifBox = <div data-tip='There are still seats remaining for this section' className='inlineBlock'>
               <NotifCheckBox seats={ this.props.section.seatsRemaining } section={ this.props.section } />
               </div>
 
