e6c00f318c1b0be7ccc10edcb167da4cdab302fb
gamburgm <mitch.gamburg@gmail.com>
2019-10-05 00:37:32 -0400

    create reasonable fetch data method

---
+++ scripts/load_data.sh
@@ -0,0 +1,2 @@
+curl -L -o public/data/v2/getTermDump/allTerms.json searchneu.com/data/v2/getTermDump/allTerms.json
+curl -L -o public/data/v2/employeeDump.json searchneu.com/data/v2/getTermDump/employeeDump.json
