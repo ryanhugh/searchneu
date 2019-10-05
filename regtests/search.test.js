cd745c36800ee92132f72be27b0ed89f0f6cf09c
gamburgm <mitch.gamburg@gmail.com>
2019-10-05 16:13:07 -0400

    add some more tests

--- regtests/search.test.js
+++ regtests/search.test.js
@@ -1,32 +1,40 @@
 import elastic from '../backend/elastic';
-import loadIndices from '../scripts/reg_tests.js';
+import Keys from '../common/Keys';
 
 // assumptions: elasticsearch running locally with prod data indexed
 
-let getFirstResult = (results) => {
+function getFirstClassResult(results) {
   return results.searchContent[0]["class"];
 }
 
 describe('elastic', () => {
-  beforeAll(async () => {
-    await loadIndices();
-  });
-
   it('returns specified class with class code query', async () => {
-    let firstResult = getFirstResult(await elastic.search('cs2500', '202010', 0, 1));
-    expect(firstResult.name).toBe("Fundamentals of Computer Science 1");
+    let firstResult = getFirstClassResult(await elastic.search('cs2500', '202010', 0, 1));
+    expect(Keys.getClassHash(firstResult)).toBe('neu.edu/202010/CS/2500');
   });
 
   it('returns specified class with name query', async () => {
-    let firstResult = getFirstResult(await elastic.search('fundamentals of computer science 2', '202010', 0, 1));
-    expect(firstResult.classId).toBe("2510");
-    expect(firstResult.subject).toBe("CS");
+    let firstResult = getFirstClassResult(await elastic.search('fundamentals of computer science 2', '202010', 0, 1));
+    expect(Keys.getClassHash(firstResult)).toBe('neu.edu/202010/CS/2510');
   });
 
   it('returns a professor if name requested', async () => {
-    expect(true).toBe(true);
+    let results = await elastic.search('mislove', '202010', 0, 1);
+    let firstResult = results.searchContent[0]["employee"];
+    expect(firstResult.name).toBe('Alan Mislove');
   });
 
-  it('does not boost labs and recitations as top results', () => {
+  it('does not place labs and recitations as top results', async () => {
+    let firstResult = getFirstClassResult(await elastic.search('cs', '202010', 0, 1));
+    expect(['Lab', 'Recitation & Discussion', 'Seminar']).not.toContain(firstResult.scheduleType);
+  });
+
+  it('aliases class names', async () => {
+    let firstResult = getFirstClassResult(await elastic.search('fundies', '202010', 0, 1));
+    expect(Keys.getClassHash(firstResult)).toBe('neu.edu/202010/CS/2500');
   });
 });
+
+// other tests I want:
+// check two aliases, maybe more than that
+// that's probably fine honestly
