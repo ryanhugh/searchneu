1bdcb7afa3b3139c6b48faff48fc3af60a498879
Da-Jin Chu <dchu@alignable.com>
2019-10-13 20:16:32 -0400

    more graphql api testing

--- backend/graphql.js
+++ backend/graphql.js
@@ -1,57 +1,67 @@
 const { ApolloServer, gql } = require('apollo-server');
+const GraphQLJSON = require('graphql-type-json');
 const { filter, find } = require('lodash');
 
 const typeDefs = gql`
+  scalar JSON
+
   type Class {
     name: String
     subject: String
     classId: Int
 
-    occurrences: [ClassOccurrence]
+    occurrence(termId: Int!): ClassOccurrence
     latestOccurrence: ClassOccurrence
+    allOccurrences: [ClassOccurrence]
   }
 
   type ClassOccurrence {
     name: String
+    desc: String
     subject: String
-    sections: String
+    classId: Int
     termId: Int
+    prereq: JSON
   }
 
   type Query {
-    class(subject: String, classId: Int): Class
-    classOccurrence(subject: String, classId: Int, termId: Int): ClassOccurrence
+    class(subject: String!, classId: Int!): Class
   }
 `;
 
 const classes = [
   {
     name: 'Fundies',
+    desc: 'Blah blah blah',
     subject: 'CS',
     classId: 2500,
     termId: 201930,
   },
   {
     name: 'Fundies',
+    desc: 'blub blub blub',
     subject: 'CS',
     classId: 2500,
     termId: 201830,
   },
   {
     name: 'OOD',
+    desc: 'OOOOOOOOOOOOOOOOOOOOOO',
     subject: 'CS',
     classId: 3500,
   },
 ];
 
 const resolvers = {
+  JSON: GraphQLJSON,
   Query: {
-    class: (parent, args) => { return find(classes, { classId: args.classId }); },
-    classOccurrence: (parent, args) => { return find(classes, args); },
+    class: (parent, args) => { return find(classes, args); },
+    // classOccurrence: (parent, args) => { return find(classes, args); },
   },
   Class: {
     latestOccurrence: (clas) => { return find(classes, { classId: clas.classId }); },
-    occurrences: (clas ) => { return filter(classes, { classId: clas.classId }); },
+    allOccurrences: (clas) => { return filter(classes, { classId: clas.classId }); },
+    occurence: (clas, args) => { return find(classes, { classId: clas.classId, termId: args.termId }); },
   },
 };
 
