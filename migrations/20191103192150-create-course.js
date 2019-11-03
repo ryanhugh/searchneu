7e0ee26ce1146d04be27bd212876a8f6e58e0f91
gamburgm <mitch.gamburg@gmail.com>
2019-11-03 15:25:03 -0500

    setting up sequelize and creating classes table

---
+++ migrations/20191103192150-create-course.js
@@ -0,0 +1,78 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Courses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      maxCredits: {
+        type: Sequelize.INTEGER
+      },
+      minCredits: {
+        type: Sequelize.INTEGER
+      },
+      desc: {
+        type: Sequelize.TEXT
+      },
+      classId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: 'classKeyIndex'
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      prettyurl: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      lastUpdateTime: {
+        type: Sequelize.DATE
+      },
+      termId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: 'classKeyIndex'
+      },
+      host: {
+        type: Sequelize.STRING
+      },
+      subject: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: 'classKeyIndex'
+      },
+      prereqs: {
+        type: Sequelize.JSON
+      },
+      coreqs: {
+        type: Sequelize.JSON
+      },
+      prereqsFor: {
+        type: Sequelize.JSON
+      },
+      optPrereqsFor: {
+        type: Sequelize.JSON
+      },
+      classAttributes: {
+        type: Sequelize.ARRAY(Sequelize.STRING)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Courses');
+  }
+};
---
+++ models/course.js
@@ -0,0 +1,25 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Course = sequelize.define('Course', {
+    maxCredits: DataTypes.INTEGER,
+    minCredits: DataTypes.INTEGER,
+    desc: DataTypes.TEXT,
+    classId: DataTypes.STRING,
+    url: DataTypes.STRING,
+    prettyurl: DataTypes.STRING,
+    name: DataTypes.STRING,
+    lastUpdateTime: DataTypes.DATE,
+    termId: DataTypes.STRING,
+    host: DataTypes.STRING,
+    subject: DataTypes.STRING,
+    prereqs: DataTypes.JSON,
+    coreqs: DataTypes.JSON,
+    prereqsFor: DataTypes.JSON,
+    optPrereqsFor: DataTypes.JSON,
+    classAttributes: Sequelize.ARRAY(Sequelize.STRING)
+  }, {});
+  Course.associate = function(models) {
+    // associations can be defined here
+  };
+  return Course;
+};
