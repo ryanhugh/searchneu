def7ba952ad7cb8474417b07449d08b88b9930af
meebs <alex@takayamasan.com>
2019-11-11 20:33:01 -0500

    resolvers - oops

---
+++ backend/graphql/resolvers/major.js
@@ -0,0 +1,26 @@
+// import magic things
+const MajorData = null;
+
+const getLatestMajorOccurrence = async (majorId) => {
+  return MajorData.findAll({
+    where: { majorId: args.majorId },
+    order: [['catalogYear', 'DESC']],
+    limit: 1
+  });
+};
+
+const resolvers = {
+  Query: {
+    major: (parent, args) => { return getLatestMajorOccurrence(args.majorId); }
+  },
+  Major: {
+    occurrence: (major, args) => {
+      return MajorData.findAll({
+        where: { majorId: major.majorId, catalogYear: String(args.year) },
+        order: [['catalogYear', 'DESC']],
+        limit: 1
+      })
+    },
+    latestOccurrence: (major) => { return getLatestMajorOccurrence(major.majorId); }
+  }
+};
