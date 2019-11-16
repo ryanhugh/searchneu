86102a0158421f66d9078bc2e1b9e1c7f341809c
gamburgm <mitch.gamburg@gmail.com>
2019-11-16 17:23:27 -0500

    small cleanup

--- backend/graphql/resolvers/major.js
+++ backend/graphql/resolvers/major.js
@@ -1,28 +1,29 @@
 import db from '../../../models/index';
+
 const MajorData = db.MajorData;
 
 const getLatestMajorOccurrence = async (majorId) => {
   return MajorData.findAll({
     where: { majorId: args.majorId },
     order: [['catalogYear', 'DESC']],
-    limit: 1
+    limit: 1,
   });
 };
 
 const resolvers = {
   Query: {
-    major: (parent, args) => { return getLatestMajorOccurrence(args.majorId); }
+    major: (parent, args) => { return getLatestMajorOccurrence(args.majorId); },
   },
   Major: {
     occurrence: (major, args) => {
       return MajorData.findAll({
         where: { majorId: major.majorId, catalogYear: String(args.year) },
         order: [['catalogYear', 'DESC']],
-        limit: 1
-      })
+        limit: 1,
+      });
     },
-    latestOccurrence: (major) => { return getLatestMajorOccurrence(major.majorId); }
-  }
+    latestOccurrence: (major) => { return getLatestMajorOccurrence(major.majorId); },
+  },
 };
 
 export default resolvers;
