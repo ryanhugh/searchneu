7cfd2b1c1ad07ffc36593fc0770b88a3dac451fc
gamburgm <mitch.gamburg@gmail.com>
2019-11-23 18:27:06 -0500

    corrected generator

--- backend/models/recordGenerator.js
+++ backend/models/recordGenerator.js
@@ -1,6 +1,7 @@
 import db from './index';
 import fs from 'fs-extra';
 import _ from 'lodash';
+import Keys from '../../common/Keys';
 
 const Professor = db.Professor;
 const Course = db.Course;
@@ -8,61 +9,52 @@ const Section = db.Section;
 const Meeting = db.Meeting;
 
 class RecordGenerator {
-    async main(termDump, profDump) {
-        _.mapValues(profDump, (prof) => {
-            this.insertProf(prof);
-        });
-        termDump.classes.forEach(aClass => {
-            this.insertClass(aClass);
-        });
-    }
+  async main(termDump, profDump) {
+    _.mapValues(profDump, (prof) => {
+      Professor.create(profInfo);
+    });
 
-    async insertProf(profInfo) {
-        const prof = await Professor.create(profInfo);
-    }
+    termDump.classes.forEach(aClass => {
+      await this.insertClass(aClass);
+    });
 
-    async insertClass(classInfo) {
-        const course = await Course.create(classInfo);
-        classInfo.sections.forEach(section => {
-            this.insertSection(course, section);
-        });
-    }
+    termDump.sections.forEach(section => {
+      await this.insertSection(section);
+    });
+  }
 
-    async insertSection(course, secInfo) {
-        secInfo.classId = course.id;
-        const section = await Section.create(secInfo);
-        
-        section.meetings.forEach(meeting => {
-            this.insertMeeting(section, meeting);
-        });
-    }
+  async insertClass(classInfo) {
+    const additionalProps = { classHash: Keys.getClassHash(classInfo) };
+    const course = await Course.create({ ...classInfo, ...additionalProps });
+  }
 
-    async insertMeeting(section, meetingInfo) {
-        meetingInfo.sectionId = section.id;
-        const meeting = await Meeting.create(meetingInfo);
-    }
+  async insertSection(secInfo) {
+    const additionalProps = { sectionHash: Keys.getSectionHash(secInfo), classHash: Keys.getClassHash(secInfo) };
+    const section = await Section.create({ ...secInfo, ...additionalProps });
+  }
 }
 
 const instance = new RecordGenerator();
 
 async function fromFile(termFilePath, empFilePath) {
-    const termExists = await fs.pathExists(termFilePath);
-    const empExists = await fs.pathExists(empFilePath);
+  const termExists = await fs.pathExists(termFilePath);
+  const empExists = await fs.pathExists(empFilePath);
 
-    if (!termExists || !empExists) {
-      macros.error('need to run scrape before indexing');
-      return;
-    }
-    const termDump = await fs.readJson(termFilePath);
-    const profDump = await fs.readJson(empFilePath);
-    instance.main(termDump, profDump);
+  if (!termExists || !empExists) {
+    macros.error('need to run scrape before indexing');
+    return;
   }
+
+  const termDump = await fs.readJson(termFilePath);
+  const profDump = await fs.readJson(empFilePath);
+  instance.main(termDump, profDump);
+}
+
+if (require.main === module) {
+  // If called directly, attempt to index the dump in public dir
+  const termFilePath = path.join(macros.PUBLIC_DIR, 'getTermDump', 'allTerms.json');
+  const empFilePath = path.join(macros.PUBLIC_DIR, 'employeeDump.json');
+  fromFile(termFilePath, empFilePath).catch(macros.error);
+}
   
-  if (require.main === module) {
-    // If called directly, attempt to index the dump in public dir
-    const termFilePath = path.join(macros.PUBLIC_DIR, 'getTermDump', 'allTerms.json');
-    const empFilePath = path.join(macros.PUBLIC_DIR, 'employeeDump.json');
-    fromFile(termFilePath, empFilePath).catch(macros.error);
-  }
-  
-  export default instance;
+export default instance;
