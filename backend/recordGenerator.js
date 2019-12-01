7c011c147a155eac483e9a858b77a5387f1aa907
gamburgm <mitch.gamburg@gmail.com>
2019-11-30 21:51:43 -0500

    working: weird sequelize timeout error

--- backend/recordGenerator.js
+++ backend/recordGenerator.js
@@ -1,5 +1,11 @@
+/*
+ * This file is part of Search NEU and licensed under AGPL3.
+ * See the license file in the root folder for details.
+ */
+
 import fs from 'fs-extra';
 import path from 'path';
+import _ from 'lodash';
 import Keys from '../common/Keys';
 import macros from './macros';
 import db from './database/models/index';
@@ -9,34 +15,41 @@ const Course = db.Course;
 const Section = db.Section;
 
 class RecordGenerator {
+  /**
+   * @param {Object} termDump object containing all class and section data, normally acquired from scrapers
+   * @param {Object} profDump object containing all professor data, normally acquired from scrapers
+   */
   async main(termDump, profDump) {
-    _.mapValues(profDump, async (prof) => {
-      await this.insertProf(prof);
+    const profPromises = _.mapValues(profDump, async (prof) => {
+      return this.insertProf(prof);
     });
 
-    // for asynchronous behavior: each of these threads can run asynchronously, but each block needs the previous one to complete (except profs, I guess?)
-    termDump.classes.forEach(async (aClass) => {
-      const res = await this.insertClass(aClass);
+    const classPromises = await termDump.classes.map(async (aClass) => {
+      return this.insertClass(aClass);
     });
 
-    termDump.sections.forEach(async (section) => {
-      await this.insertSection(section);
+    const secPromises = termDump.sections.map(async (section) => {
+      return this.insertSection(section);
     });
+
+    await Promise.all(profPromises);
+    await Promise.all(classPromises);
+    await Promise.all(secPromises);
   }
 
   async insertProf(profInfo) {
     delete profInfo.id;
-    await Professor.create(profInfo);
+    return Professor.create(profInfo);
   }
 
   async insertClass(classInfo) {
     const additionalProps = { id: Keys.getClassHash(classInfo), minCredits: Math.floor(classInfo.minCredits), maxCredits: Math.floor(classInfo.maxCredits) };
-    const val = await Course.create({ ...classInfo, ...additionalProps });
+    return Course.create({ ...classInfo, ...additionalProps });
   }
 
   async insertSection(secInfo) {
     const additionalProps = { id: Keys.getSectionHash(secInfo), classHash: Keys.getClassHash(secInfo) };
-    Section.create({ ...secInfo, ...additionalProps });
+    return Section.create({ ...secInfo, ...additionalProps });
   }
 }
 
@@ -53,7 +66,7 @@ async function fromFile(termFilePath, empFilePath) {
 
   const termDump = await fs.readJson(termFilePath);
   const profDump = await fs.readJson(empFilePath);
-  instance.main(termDump, profDump);
+  await instance.main(termDump, profDump);
 }
 
 if (require.main === module) {
