8134589b9dcfecce2f1696c9a0d33ab4527c2490
dajinchu <dajinchu@gmail.com>
2020-01-03 17:55:09 -0500

    meetingparser test consistency

--- backend/scrapers/classes/parsersxe/meetingParser.js
+++ backend/scrapers/classes/parsersxe/meetingParser.js
@@ -92,17 +92,10 @@ function days(meetingTime) {
 }
 
 /**
- * @param facultyMeetingTimes the output from
- * https://jennydaman.gitlab.io/nubanned/dark.html#searchresults-meeting-times-get
- * @return {*}
- * see https://github.com/jennydaman/searchneu/blob/1f86ac0a199cd1b59620beea8d58dddd9b7f0308/backend/tests/dataxe/getFacultyMeetingTimes/htlh2200.searchneu.json
- * for an example.
+ * @param facultyMeetingTimes meetingsFaculty object from searchresults, or /searchResults/getFacultyMeetingTimes
  */
-function meetings(xeResponse) {
-  if (Array.isArray(xeResponse.fmt)) {
-    xeResponse = xeResponse.fmt;
-  }
-  return xeResponse.map((m) => {
+function meetings(facultyMeetingTimes) {
+  return facultyMeetingTimes.map((m) => {
     const meetingTime = m.meetingTime;
     return {
       startDate: mmddyyyyToDaysSinceEpoch(meetingTime.startDate),
