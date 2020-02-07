323a3bdd58c2531999a15c658a5ddf1d249d638e
Amiel Monasterial <amielmon@gmail.com>
2020-02-06 21:27:52 -0500

    fix

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -19,6 +19,7 @@ import macros from './macros';
 import ResultsLoader from './ResultsLoader';
 import logo from './images/logo.svg';
 import boston from './images/boston.svg';
+import TopSearchBar from './TopSearchBar';
 
 // This file is the main starting point for the frontend
 // It calls into a bunch of different files
@@ -144,6 +145,8 @@ class Home extends React.Component {
 
     // Log the initial search or pageview.
     this.logSearch(this.state.searchQuery);
+
+    this.searchPageVisible = false;
   }
 
   componentDidMount() {
@@ -270,6 +273,9 @@ class Home extends React.Component {
 
       this.onSearchDebounced(event.target.value);
     }
+    this.setState({ headerTitleVisible: false });
+    this.searchPageVisible = true;
+    this.onSearchDebounced(event.target.value);
 
     this.searchFromUserAction(event);
   }
@@ -520,8 +526,7 @@ class Home extends React.Component {
       } else {
         topHeaderStyle.transform = 'translateY(-50%) translateY(292px)';
       }
-
-      resultsContainerStyle.transform = `translateY(-${window.innerHeight - 310}px)`;
+      resultsContainerStyle.transform = `translateY(-${window.innerHeight}px)`;
 
       // Without this line, there would be a lot of whitespace after the results.
       resultsContainerStyle.marginBottom = `${310 - window.innerHeight}px`;
@@ -592,18 +597,29 @@ class Home extends React.Component {
     // Actually it only removes the extra whitespace on chrome. Need to come up with a better solution for other browsers.
     return (
       <div className={ containerClassnames }>
-
-        <a target='_blank' rel='noopener noreferrer' href='https://github.com/sandboxnu/searchneu' className='githubCornerContainer'>
-          {/* eslint-disable max-len */}
-          <svg width='80' height='80' viewBox='0 0 250 250'>
+        { console.log('this.onClick', this.onClick) }
+        {/* <a target='_blank' rel='noopener noreferrer' href='https://github.com/sandboxnu/searchneu' className='githubCornerContainer'> */}
+        {/* eslint-disable max-len */}
+        {/* <svg width='80' height='80' viewBox='0 0 250 250'>
             <path d='M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z' />
             <path d='M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2' fill='currentColor' className='octopusArm' />
             <path d='M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z' fill='currentColor' />
-          </svg>
-          {/* eslint-enable max-len */}
-        </a>
+          </svg> */}
+        {/* eslint-enable max-len */}
+        {/* </a>
 
-        <img src={ logo } className='logo' alt='logo' onClick={ this.onLogoClick } />
+        <img src={ logo } className='logo' alt='logo' onClick={ this.onLogoClick } /> */}
+        {this.searchPageVisible && (
+        <TopSearchBar
+          onClick={ this.onClick }
+          onKeyDown={ this.onKeyDown }
+          searchQuery={ this.state.searchQuery }
+          inputElement={ this.inputElement }
+          selectedTermId={ this.state.selectedTermId }
+          termDropDownOptions={ termDropDownOptions }
+          onTermdropdownChange={ this.onTermdropdownChange }
+        />
+        )}
 
         <div className='bostonContainer' style={ bostonContainerStyle }>
           <img src={ boston } className='boston' alt='logo' />
@@ -622,6 +638,7 @@ class Home extends React.Component {
             }) }
             style={ topHeaderStyle }
           >
+            { !this.searchPageVisible && (
             <div className='centerTextContainer'>
               <h1 className='title'>
                 Search For Northeastern
@@ -661,6 +678,7 @@ class Home extends React.Component {
 
               {hitEnterToSearch}
             </div>
+            )}
           </div>
         </div>
 
