b42a127f6838b45f609281f8eba2aac8a4a7449d
dajinchu <dajinchu@gmail.com>
2020-02-24 03:52:17 -0500

    import

--- frontend/components/pages/Home.js
+++ frontend/components/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import cx from 'classnames';
 import { Dropdown } from 'semantic-ui-react';
