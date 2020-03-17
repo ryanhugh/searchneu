5293e700d1dfa22c48edd5324cbf0e5cf9696f4b
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2020-03-16 21:55:59 -0400

    Build custom toggle switch.

--- frontend/components/ResultsPage/OnlineFilter.tsx
+++ frontend/components/ResultsPage/OnlineFilter.tsx
@@ -1,21 +1,50 @@
-import React from 'react';
-import { Checkbox } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import '../../css/_Filters.scss';
 
 interface OnlineFilterProps {
   active: boolean,
   setActive: (a:boolean)=>void
 }
 
-
-
-export default function OnlineFilter({ active, setActive }: OnlineFilterProps) {
+const ToggleSwitch = ({ active, setActive }) => {
   return (
-    <Checkbox
+    <>
+      <input
+        checked={ active }
+        onChange={ setActive }
+        className='react-switch-checkbox'
+        id='react-switch-new'
+        type='checkbox'
+      />
+      <label
+        style={{ background:active && '#AE4543' }} // Color is currently same as darker red in Figma
+        className='react-switch-label'
+        htmlFor='react-switch-new'
+      >
+        <span className='react-switch-button' />
+      </label>
+    </>
+  )
+};
+
+/*
+<Checkbox
       toggle
       onChange={ () => {
         setActive(!active);
       } }
       checked={ active }
     />
+ */
+
+export default function OnlineFilter({ active, setActive }: OnlineFilterProps) {
+  const [value, setValue] = useState(false);
+  return (
+    <div className='onlineToggle'>
+      <ToggleSwitch
+        active={ value }
+        setActive={ () => setValue(!value) }
+      />
+    </div>
   );
 }
